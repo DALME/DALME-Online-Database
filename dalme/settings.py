097b54f2eaf515ff01aeff29b5ead730939ffbf4
jaguillette <jguillette@fas.harvard.edu>
2017-05-31 10:33:52 -0400

    setting up remote mysql

--- dalme/settings.py
+++ dalme/settings.py
@@ -79,9 +79,15 @@ WSGI_APPLICATION = 'dalme.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': os.path.join(BASE_DIR, 'db.cnf')
+        }
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
