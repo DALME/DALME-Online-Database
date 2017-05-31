daec2d0b0c32e0e44f7325b97ebf6230d9f2e16a
jaguillette <jguillette@fas.harvard.edu>
2017-05-31 16:57:20 -0400

    changed timezone

--- dalme/settings.py
+++ dalme/settings.py
@@ -109,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/New_York'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
