1a7b5f562fb556bf02e4bced38d2cabfa9a5ad0d
jaguillette <jaguillette@gmail.com>
2017-06-05 12:14:00 -0400

    added auth check for homepage edit link

--- platonic_concepts/views.py
+++ platonic_concepts/views.py
@@ -10,7 +10,12 @@ import re
 def index(request):
     latest_concepts = PlatonicConcept.objects.order_by('-modification_timestamp')[:5]
     relationships = Relationship.objects.order_by('-modification_timestamp')[:5]
-    context = {'page_title':'DALME | Home', 'concepts': latest_concepts, 'relationships': relationships}
+    context = {
+        'page_title':'DALME | Home',
+        'concepts': latest_concepts,
+        'relationships': relationships,
+        'authenticated': request.user.is_authenticated
+    }
     return render(request, 'platonic_concepts/index.html', context)
 
 def concept_detail(request, concept_id):
