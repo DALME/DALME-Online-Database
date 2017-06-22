efd4ddc3090884e172dfa351f0603e073f0d04d6
Gabe Pizzorno <pizzorno@fas.harvard.edu>
2017-06-22 13:47:10 -0400

    dropdown test page 1 fix

--- dalme_app/urls.py
+++ dalme_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^concept/(?P<concept_id>[0-9a-z-]+)/$', views.concept_detail, name='concept_detail')
+    url(r'^concept/(?P<concept_id>[0-9a-z-]+)/$', views.concept_detail, name='concept_detail'),
+    url(r'^dropdown$', views.dropdown_test, name='dropdown_test')
 ]
