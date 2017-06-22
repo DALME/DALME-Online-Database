07aab1772b1480800747c406ad75599a7586ea0a
Gabe Pizzorno <pizzorno@fas.harvard.edu>
2017-06-22 14:01:37 -0400

    another go at it

--- dalme_app/views.py
+++ dalme_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 
-from .models import PlatonicConcept, Relationship
+from .models import PlatonicConcept, Relationship, Sources
 
 import requests
 import re
