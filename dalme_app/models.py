ead4f47a78c8e01db4a9b2ffc4d685e0282faa6d
Gabe Pizzorno <pizzorno@fas.harvard.edu>
2017-07-07 16:31:25 -0400

    new database model
    completely refactored database

--- dalme_app/models.py
+++ dalme_app/models.py
@@ -8,56 +8,134 @@ def make_uuid():
     the_id = uuid4().hex
     return the_id
 
-class PlatonicConcept(DalmeBaseModel):
-    term = models.CharField(max_length=255, null=True, blank=True)
-    getty_term = models.CharField(max_length=255, null=True, blank=True)
-    comments = models.TextField(null=True, blank=True)
-    def __str__(self):
-        return self.term
+class external_lemmata(DalmeBaseModel):
 
-class Relationship(DalmeBaseModel):
-    source = models.ForeignKey(PlatonicConcept, related_name='source')
-    target = models.ForeignKey(PlatonicConcept, related_name='target')
-    relationship = models.CharField(max_length=36)
+
+class external_lemma_attributes(DalmeBaseModel):
+    subject = models.ForeignKey('external_lemmata', related_name='subject')
+    predicate = models.CharField(max_length=32)
+    _object = models.CharField(max_length=255)
     def __str__(self):
-        representation = "{} - {} -> {}".format(self.source,self.relationship,self.target)
+        representation = "{} -> {} -> {}".format(self.subject,self.predicate,self._object)
         return representation
 
-class SourceType(DalmeBaseModel):
-    comments = models.TextField(null=True, blank=True)
-    type_csl = models.CharField(max_length=255, null=True, blank=True)
-    type_zotero = models.CharField(max_length=255, null=True, blank=True)
-    dropdown_content = models.CharField(max_length=255, null=True, blank=True)
-    def __str__(self):
-        if self.type_csl:
-            return self.type_csl
-        elif self.type_zotero:
-            return self.type_zotero
-        else:
-            return self.dropdown_content
 
-class SourceAttributeType(DalmeBaseModel):
-    type_dublin_core = models.CharField(max_length=255, null=True, blank=True)
-    type_csl = models.CharField(max_length=255, null=True, blank=True)
-    type_zotero = models.CharField(max_length=255, null=True, blank=True)
-    comments = models.TextField(null=True, blank=True)
-    dropdown_content = models.CharField(max_length=255, null=True, blank=True)
-    def __str__(self):
-        return self.dropdown_content
+class phrases(DalmeBaseModel):
+    parent_phrase = models.CharField(max_length=36)
+    _type = models.CharField(max_length=36)
+    platonic_concept = models.CharField(max_length=36)
 
-class Source(DalmeBaseModel):
-    source_type = models.ForeignKey(SourceType)
-    parent_source = models.ForeignKey("self", related_name='source_parent', null=True, blank=True)
-    dropdown_content = models.CharField(max_length=255, null=True, blank=True)
-    comments = models.TextField(null=True, blank=True)
-    def __str__(self):
-        return self.dropdown_content
 
-class SourceAttribute(DalmeBaseModel):
-    source = models.ForeignKey(Source)
-    type = models.ForeignKey(SourceAttributeType)
-    content = models.CharField(max_length=255, null=True, blank=True)
+class source_attributes(DalmeBaseModel):
+    subject = models.CharField(max_length=32)
+    predicate = models.CharField(max_length=32)
+    _object = models.CharField(max_length=255)
     comments = models.TextField(null=True, blank=True)
     def __str__(self):
-        representation = "{}: {}".format(self.type,self.content)
+        representation = "{} -> {} -> {}".format(self.subject,self.predicate,self._object)
+        return representation
+
+
+class sources(DalmeBaseModel):
+    temp = models.CharField(max_length=255)
+
+
+class superlemmata(DalmeBaseModel):
+
+
+class superlemma_components(DalmeBaseModel):
+    subject = models.ForeignKey('superlemmata', related_name='subject')
+    predicate = models.CharField(max_length=32)
+    _object = models.CharField(max_length=255)
+    def __str__(self):
+        representation = "{} -> {} -> {}".format(self.subject,self.predicate,self._object)
+        return representation
+
+
+class concepts(DalmeBaseModel):
+
+
+class concept_components(DalmeBaseModel):
+    subject = models.ForeignKey('concepts', related_name='subject')
+    predicate = models.CharField(max_length=32)
+    _object = models.CharField(max_length=255)
+    def __str__(self):
+        representation = "{} -> {} -> {}".format(self.subject,self.predicate,self._object)
+        return representation
+
+
+class inventories(DalmeBaseModel):
+
+
+class inventory_versions(DalmeBaseModel):
+    inventory = models.ForeignKey('inventories', related_name='inventory')
+
+
+class leaves(DalmeBaseModel):
+
+
+class leaf_images(DalmeBaseModel):
+    leaf = models.ForeignKey('leaves', related_name='leaf')
+
+
+class lines(DalmeBaseModel):
+    leaf = models.ForeignKey('leaves', related_name='leaf')
+    inventory_version = models.ForeignKey('inventory_versions', related_name='inventory_version')
+
+
+class tokens(DalmeBaseModel):
+    token = models.CharField(max_length=255)
+    line = models.ForeignKey('lines', related_name='line')
+    def __str__(self):
+        return self.token
+
+
+class phrase_parts(DalmeBaseModel):
+    token = models.ForeignKey('tokens', related_name='token')
+    phrase = models.ForeignKey('phrases', related_name='phrase')
+
+
+class inventory_attributes(DalmeBaseModel):
+
+
+class predicates(DalmeBaseModel):
+    uri = models.CharField(max_length=255)
+    def __str__(self):
+        return self.uri
+
+
+class predicate_labels(DalmeBaseModel):
+    predicate = models.ForeignKey('predicates', related_name='predicate')
+    language = models.AutoField()
+    label = models.CharField(max_length=255)
+    def __str__(self):
+        representation = "@{}:{}".format(self.language,self.label)
+        return representation
+
+
+class references(DalmeBaseModel):
+    foreign_id = models.CharField(max_length=36)
+    _type = models.CharField(max_length=36)
+    sources_id = models.CharField(max_length=36)
+    comments = models.TextField(null=True, blank=True)
+
+
+class repository(DalmeBaseModel):
+
+
+class reference_attributes(DalmeBaseModel):
+    subject = models.ForeignKey('references', related_name='subject')
+    predicate = models.CharField(max_length=32)
+    _object = models.CharField(max_length=32)
+    def __str__(self):
+        representation = "{} -> {} -> {}".format(self.subject,self.predicate,self._object)
+        return representation
+
+
+class repository_attributes(DalmeBaseModel):
+    subject = models.ForeignKey('references', related_name='subject')
+    predicate = models.CharField(max_length=32)
+    _object = models.CharField(max_length=32)
+    def __str__(self):
+        representation = "{} -> {} -> {}".format(self.subject,self.predicate,self._object)
         return representation
