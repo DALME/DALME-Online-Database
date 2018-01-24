4de15c303963fb9248a5e8f25ac1f6be253789ee
jaguillette <jaguillette@gmail.com>
2018-01-24 09:48:07 -0500

    fixed check for admin account

--- dalme_app/management/commands/createsu.py
+++ dalme_app/management/commands/createsu.py
@@ -5,5 +5,5 @@ import os
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        if not User.objects.filter(username="admin").exists():
+        if not User.objects.filter(username="jguillette").exists():
             User.objects.create_superuser(os.environ['DJANGO_SUPERUSER_NAME'], os.environ['DJANGO_SUPERUSER_EMAIL'], os.environ['DJANGO_SUPERUSER_PASSWORD'])
