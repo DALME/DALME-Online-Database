665fe15d0d2eda5df411b5cae188879b6796fc80
jaguillette <jaguillette@gmail.com>
2018-01-24 11:56:19 -0500

    re-activating django cms app

--- dalme/settings.py
+++ dalme/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.admindocs',
     'django.contrib.sites',
-    # 'cms',
+    'cms',
     'menus',
     'treebeard',
     'sekizai',
