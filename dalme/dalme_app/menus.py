d26216f868cb08c24fcc776ea8203fdd95900fc9
jaguillette <jaguillette@gmail.com>
2018-02-05 17:01:42 -0500

    commenting run on menu changes

--- dalme/dalme_app/menus.py
+++ dalme/dalme_app/menus.py
@@ -23,45 +23,88 @@ def sidebar_menu(template='sidebar_default.json'):
 
     All properties default to `None`, so if you don't want to include any
     element, just leave out that key."""
+
+    # Get template from default location in dalme_app/templates/menus and read it
     template = os.path.join('dalme','dalme_app','templates','menus',template)
     with open(template, 'r') as fp:
         menu = json.load(fp)
 
+    # Declare output string
     _output = ''
 
+    # Create menu by iterating through items in json file and appending to output
     for item in menu:
         _output += sidebar_menu_item(_output,**item)
 
+    # Return output as part of a list, because renderer expects to iterate
     return [_output]
 
 LEVEL_LOOKUP = ['nav-second-level', 'nav-third-level', 'nav-fourth-level', 'nav-fifth-level']
-def sidebar_menu_item(wholeMenu,depth=0,text=None,iconClass=None,link=None,counter=None,children=None,section=None):
+def sidebar_menu_item(wholeMenu,depth=0,text=None,iconClass=None,link=None,counter=None,section=None,children=None):
+    """
+    Generates a menu item and incorporates it into `whoeleMenu`. This function
+    calls itself to recurse through hierarchies of menus, and uses the
+    `LEVEL_LOOKUP` variable with the `depth` parameter to give subheadings the
+    right class.
+
+    :param wholeMenu: The entire collection of menu items to which the current
+        item will be added
+    :param depth: The level of depth within the menu of the current item.
+        Defaults to 0, but is incremented upon recursion
+    :param text: Text of the menu item
+    :param iconClass: Class for the Font Awesome icon to accompany menu item
+    :param link: Link to be used as the href for the menu item
+    :param counter: What kind of thing to count and add to the menu. The value
+        of this parameter will be passed to the `functions.get_count()` function
+        and the return value will be incorporated into the menu item
+    :param section: If this parameter is set to true, the menu item will be
+        given a class for a menu section header.
+    :param children: List of child menu items to appear under this item. Items
+        should be dictionaries, with keys corresponding to the parameters of
+        this function
+    """
     currentItem = '<li '
+    # If this item is a section header and the whole menu is empty, this is the
+    # first section header
     if section and wholeMenu == '':
         currentItem += 'class="sidebar-section-first"'
     elif section:
         currentItem += 'class="sidebar-section"'
     currentItem += '>'
+
     if link:
         currentItem += '<a href="{}">'.format(link)
+
     if iconClass:
         currentItem += '<i class="fa {} fa-fw"></i> '.format(iconClass)
+
     if text:
         currentItem += text
+
     if counter:
         counter = functions.get_count(counter)
         currentItem += '<div class="menu-counter">{}</div>'.format(counter)
+
     if children:
+        # If this item has children, append an arrow to show that
         currentItem += '<span class="fa arrow"></span>'
+
     if link:
+        # Close the anchor tag if it was opened
         currentItem += '</a>'
+
     if children:
+        # If there are child items, start a new unordered list based on the
+        # depth. If depth is not defined in LEVEL_LOOKUP, that <ul> doesn't get
+        # a class, so it might look weird.
         try:
             currentItem += '<ul class="nav {}">'.format(LEVEL_LOOKUP[depth])
         except IndexError:
             print(depth)
             currentItem += '<ul class="nav">'
         for child in children:
+            # For each child item, provide the parameters it defines to this
+            # function, incrementing the depth
             currentItem += sidebar_menu_item(currentItem,depth=depth+1,**child)
         currentItem += '</ul>'
     currentItem += '</li>'
