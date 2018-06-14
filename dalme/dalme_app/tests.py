9c24a921cc0734b55391ab69965d8a40d7ce05a0
jaguillette <jaguillette@gmail.com>
2018-06-14 14:46:49 -0400

    first test

--- dalme/dalme_app/tests.py
+++ dalme/dalme_app/tests.py
@@ -1,1 +1,8 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+
+class HomepageTestCase(TestCase):
+    def test_load_homepage(self):
+        c = Client()
+        response = c.get(reverse('dashboard'), follow=True)
+        self.assertEqual(response.status_code,200)
