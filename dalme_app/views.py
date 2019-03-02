7d292c2ea6db480846b437f767a4600dc53bed73
Gabe Pizzorno <pizzorno@fas.harvard.edu>
2019-03-02 16:20:17 -0500

    test

--- dalme_app/views.py
+++ dalme_app/views.py
@@ -4,6 +4,7 @@ Functions for managing views
 
 from django.contrib import messages
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
