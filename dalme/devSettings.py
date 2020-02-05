bdee9bc21c4330e8cddad6b87ccf9c935a165039
jhrr <jhrr@users.noreply.github.com>
2020-02-05 17:06:18 +0100

    Update README with missing details after initial project bootstrap

--- dalme/devSettings.py
+++ dalme/devSettings.py
@@ -251,8 +251,6 @@ else:
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-
-
 HAYSTACK_CONNECTIONS = {
     'default': {
         'ENGINE': 'haystack.backends.elasticsearch2_backend.Elasticsearch2SearchEngine',
