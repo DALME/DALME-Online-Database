828254faf5288c741ba541e072f05e9b0539bd83
jhrr <jhrr@users.noreply.github.com>
2020-02-27 00:08:14 +0100

    Wire-up public source list view and templates

--- dalme_public/urls.py
+++ dalme_public/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = 'dalme_public'
 urlpatterns = [
+    path('sources/', views.SourceList.as_view(), name='source_list'),
     path('sources/<uuid:pk>/', views.SourceDetail.as_view(), name='source_detail'),
 ]
--- dalme_public/views.py
+++ dalme_public/views.py
@@ -1,7 +1,13 @@
 from django.http import Http404
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 
 from dalme_app.models import Source, Source_pages
+
+
+class SourceList(ListView):
+    model = Source
+    template_name = 'dalme_public/source_list.html'
+    paginate_by = 25
 
 
 class SourceDetail(DetailView):
@@ -12,6 +18,7 @@ class SourceDetail(DetailView):
         context = super().get_context_data(**kwargs)
 
         page_qs = self.object.pages.all().order_by('order')
+
         if self.request.GET.get('page'):
             index = int(self.request.GET.get('page')) - 1
             try:
@@ -20,7 +27,15 @@ class SourceDetail(DetailView):
                 raise Http404()
         else:
             page = page_qs.first()
-        context.update({'page': page, 'page_id': getattr(page, 'pk', False)})
+        context.update({
+            'page': page,
+            'page_id': getattr(page, 'pk', False),
+        })
+
+        related = [
+            (page, f"?page={idx}") for idx, page in enumerate(page_qs, 1)
+        ]
+        context.update({'related': related})
 
         context.update({'transcription': None})
         if page:
@@ -31,6 +46,8 @@ class SourceDetail(DetailView):
                     split = transcription.transcription.split('\n')
                     context.update({'transcription': split})
 
-        context.update({'parent': self.object.parent})
-        context.update({'type': self.object.type})
+        context.update({
+            'parent': self.object.parent,
+            'type': self.object.type
+        })
         return context
