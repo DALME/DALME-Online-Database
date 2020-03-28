8fab7c5235c5a634f89e42426b2dbee3e6c7f1e5
jhrr <jhrr@users.noreply.github.com>
2020-03-28 20:57:14 +0100

    Develop public data models to alias app Sets

--- dalme_public/models.py
+++ dalme_public/models.py
@@ -2,41 +2,86 @@ from django.db import models
 
 from solo.models import SingletonModel
 
+from dalme_app.models import Set as DALMESet, Source
 
-class ContentPage(models.Model):
+
+class SetAliasPage(models.Model):
+    class Meta:
+        abstract = True
+
+    @property
+    def description(self):
+        return self.source_set.description
+
+    @property
+    def alias_type(self):
+        return self.source_set.source_type
+
+    @property
+    def sources(self):
+        if self.source_set:
+            return self.source_set.members.all()
+        return Source.objects.none()
+
+    def clean(self):
+        if self.source_type != self.alias_type:
+            mismatch = f'{self.source_type} != {self.alias_type}'
+            raise ValidationError(
+                f'{self._meta.label}.source_type mismatch: {mismatch}'
+            )
+
+
+class HomePage(SingletonModel):
     name = models.CharField(max_length=255)
     description = models.TextField(null=True, blank=True)
 
     class Meta:
-        abstract = True
+        verbose_name = "Home"
 
     def __str__(self):
         return self.name
 
 
-class HomePage(ContentPage, SingletonModel):
-    class Meta:
-        verbose_name = "Home"
+class Collection(SetAliasPage):
+    set_type = DALMESet.CORPUS
 
-
-class Collection(ContentPage):
+    name = models.CharField(max_length=255)
     home = models.ForeignKey(
         'dalme_public.HomePage',
         related_name='collections',
-        blank=False,
-        null=False,
         on_delete=models.CASCADE
     )
+    source_set = models.ForeignKey(
+        'dalme_app.Set',
+        related_name='public_collections',
+        null=True,
+        blank=True,
+        on_delete=models.PROTECT
+    )
+
+    def __str__(self):
+        return self.name
 
 
-class Set(ContentPage):
+class Set(SetAliasPage):
+    set_type = DALMESet.COLLECTION
+
+    name = models.CharField(max_length=255)
     collection = models.ForeignKey(
         'dalme_public.Collection',
         related_name='sets',
-        blank=False,
-        null=False,
         on_delete=models.CASCADE
     )
+    source_set = models.ForeignKey(
+        'dalme_app.Set',
+        related_name='public_sets',
+        null=True,
+        blank=True,
+        on_delete=models.PROTECT
+    )
+
+    def __str__(self):
+        return self.name
 
 
 class MicroEssay:
