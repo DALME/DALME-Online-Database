6cc36243064adcd83cce9d75c81803a161f5a5eb
Jacob Wegner <jacobwegner@gmail.com>
2020-05-06 15:49:49 -0500

    configure django storages explicitly
    boto3 will read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY env vars
    if set, but if we set them explicitly in settings, it will be easier
    to separate the S3 credentials from other AWS credentials used by the
    application

--- dalme/devSettings.py
+++ dalme/devSettings.py
@@ -310,9 +310,16 @@ USE_TZ = True
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 if 'AWS_STORAGE_BUCKET_NAME' in os.environ:
+    AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+    AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
     DEFAULT_FILE_STORAGE = 'dalme.storage_backends.MediaStorage'
+
+    AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
     MEDIA_URL = 'https://%s/media/' % AWS_S3_CUSTOM_DOMAIN
+
     AWS_DEFAULT_ACL = None
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 
 # Extra places for collectstatic to find static files.
 # STATICFILES_DIRS = [
