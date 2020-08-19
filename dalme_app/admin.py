0734fc7d9a9d822605e8eabac0bfb927fcfbebcc
Gabe Pizzorno <pizzorno@fas.harvard.edu>
2020-08-19 17:06:21 -0400

    periodic tasks setup

--- dalme_app/admin.py
+++ dalme_app/admin.py
@@ -8,10 +8,3 @@ from django.contrib.auth.models import Permission
 from .models import *
 
 admin.site.register(Permission)
-
-class DalmeBaseAdmin(admin.ModelAdmin):
-    def save_model(self, request, obj, form, change):
-        if not change:
-            obj.creation_username = request.user.username
-        obj.modification_username = request.user.username
-        obj.save()
