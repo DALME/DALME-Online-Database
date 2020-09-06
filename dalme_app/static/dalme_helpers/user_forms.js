d83484893d8d2aad40cc228d765dc73f5d077425
Gabe Pizzorno <pizzorno@fas.harvard.edu>
2020-09-06 18:24:44 -0400

    rewrote user management forms

--- dalme_app/static/dalme_helpers/user_forms.js
+++ dalme_app/static/dalme_helpers/user_forms.js
@@ -1,24 +1,24 @@
-function user_form() {
-  dt_editor.on('open.dalme', function( e, mode, action ) { change_form(e, action) });
-  dt_editor.on('close.dalme', function( e ) { change_form(e) });
+function user_forms_load() {
+
+}
+
+function user_forms_init() {
   dt_editor.on('submitSuccess.dalme', function(e, json, data, action) { location.reload(); });
-  password_state = 'divs';
 }
 
 function change_form(e, action) {
   if (e.type == 'open') {
     if (action == 'edit') {
       toggle_password_button(e);
-      dt_editor.on('submitSuccess', function(e, json, data, action) { toastr.success('The user was updated succesfully.') });
-    } else if (action == 'create') {
-      dt_editor.on('submitSuccess', function(e, json, data, action) { toastr.success('The user was created succesfully.') });
-      $(dt_editor.field('user.last_name').node()).find('input').on('change.dalme', function() { suggest_fullname() });
-      $(dt_editor.field('user.email').node()).find('input').on('change.dalme', function() { suggest_username() });
+    }
+    if (action == 'create') {
+      $(dt_editor.field('last_name').node()).find('input').on('change.dalme', function() { suggest_fullname() });
+      $(dt_editor.field('email').node()).find('input').on('change.dalme', function() { suggest_username() });
     }
   } else if (e.type == 'close') {
         toggle_password_button(e);
-        $(dt_editor.field('user.last_name').node()).find('input').off('change.dalme');
-        $(dt_editor.field('user.email').node()).find('input').off('change.dalme');
+        $(dt_editor.field('last_name').node()).find('input').off('change.dalme');
+        $(dt_editor.field('email').node()).find('input').off('change.dalme');
   }
 }
 
@@ -40,7 +40,7 @@ function reset_password() {
 }
 
 function toggle_password_button(e) {
-  var container = $(dt_editor.field('user.password').node());
+  var container = $(dt_editor.field('password').node());
   var label = $(container).find('label');
   var button = '<div class="col-lg-8"><button class="btn btn-light ml-0" type="button" id="reset_password">Reset password</button></div>';
   if (e.type == 'open' && typeof divs == 'undefined') { divs = $(container).children('div') };
@@ -59,8 +59,8 @@ function toggle_password_button(e) {
 }
 
 function suggest_fullname() {
-  var first_name = $(dt_editor.field('user.first_name').node()).find('input').val();
-  var last_name = $(dt_editor.field('user.last_name').node()).find('input').val();
+  var first_name = $(dt_editor.field('first_name').node()).find('input').val();
+  var last_name = $(dt_editor.field('last_name').node()).find('input').val();
   if (first_name != '' && last_name != '') {
     var suggestion = first_name+' '+last_name;
     $(dt_editor.field('full_name').node()).find('input').val(suggestion);
@@ -68,9 +68,9 @@ function suggest_fullname() {
 }
 
 function suggest_username() {
-  var email = $(dt_editor.field('user.email').node()).find('input').val();
+  var email = $(dt_editor.field('email').node()).find('input').val();
   if (email != '' && email.includes("@")) {
     var suggestion = email.split("@")[0];
-    $(dt_editor.field('user.username').node()).find('input').val(suggestion);
+    $(dt_editor.field('username').node()).find('input').val(suggestion);
   }
 }
