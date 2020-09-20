c00cc31ff822dd9f95c6200a9d772c6a615f96db
Gabe Pizzorno <pizzorno@fas.harvard.edu>
2020-09-20 16:57:41 -0400

    cleaned up url files

--- dalme_app/urls.py
+++ dalme_app/urls.py
@@ -6,77 +6,81 @@ from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 from django.http import HttpResponse
 from rest_framework import routers
-from dalme_app import apis
-from dalme_app import web_apis
+from dalme_app import api
+from dalme_app import api_web
 from maintenance_mode import urls as maintenance_mode_urls
 
 router = routers.DefaultRouter()
-router.register(r'sources', apis.Sources, basename='sources')
-router.register(r'users', apis.Users, basename='users')
-router.register(r'transcriptions', apis.Transcriptions, basename='transcriptions')
-router.register(r'images', apis.Images, basename='images')
-router.register(r'pages', apis.Pages, basename='pages')
-router.register(r'tasks', apis.Tasks, basename='tasks')
-router.register(r'tasklists', apis.TaskLists, basename='tasklists')
-router.register(r'sets', apis.Sets, basename='sets')
-router.register(r'attribute_types', apis.AttributeTypes, basename='attribute_types')
-router.register(r'content_types', apis.ContentTypes, basename='content_types')
-router.register(r'content_classes', apis.ContentClasses, basename='content_classes')
-router.register(r'languages', apis.Languages, basename='languages')
-router.register(r'async_tasks', apis.AsynchronousTasks, basename='async_tasks')
-router.register(r'attributes', apis.Attributes, basename='attributes')
-router.register(r'countries', apis.Countries, basename='countries')
-router.register(r'locales', apis.Locales, basename='locales')
-router.register(r'attachments', apis.Attachments, basename='attachments')
-router.register(r'tickets', apis.Tickets, basename='tickets')
-router.register(r'comments', apis.Comments, basename='comments')
-router.register(r'workflow', apis.WorkflowManager, basename='workflow')
-router.register(r'rights', apis.Rights, basename='rights')
-router.register(r'configs', apis.Configs, basename='configs')
-router.register(r'choices', apis.Choices, basename='choices')
-router.register(r'groups', apis.Groups, basename='groups')
+router.register(r'agents', api.Agents, basename='agents')
+router.register(r'async-tasks', api.AsynchronousTasks, basename='async_tasks')
+router.register(r'attributes', api.Attributes, basename='attributes')
+router.register(r'attribute_types', api.AttributeTypes, basename='attribute_types')
+router.register(r'attachments', api.Attachments, basename='attachments')
+router.register(r'choices', api.Choices, basename='choices')
+router.register(r'comments', api.Comments, basename='comments')
+router.register(r'configs', api.Configs, basename='configs')
+router.register(r'content-classes', api.ContentClasses, basename='content_classes')
+router.register(r'content-types', api.ContentTypes, basename='content_types')
+router.register(r'countries', api.Countries, basename='countries')
+router.register(r'groups', api.Groups, basename='groups')
+router.register(r'images', api.Images, basename='images')
+router.register(r'languages', api.Languages, basename='languages')
+router.register(r'locales', api.Locales, basename='locales')
+router.register(r'pages', api.Pages, basename='pages')
+router.register(r'rights', api.Rights, basename='rights')
+router.register(r'sets', api.Sets, basename='sets')
+router.register(r'sources', api.Sources, basename='sources')
+router.register(r'tasks', api.Tasks, basename='tasks')
+router.register(r'tasklists', api.TaskLists, basename='tasklists')
+router.register(r'tickets', api.Tickets, basename='tickets')
+router.register(r'transcriptions', api.Transcriptions, basename='transcriptions')
+router.register(r'users', api.Users, basename='users')
+router.register(r'workflow', api.WorkflowManager, basename='workflow')
 
 web_router = routers.DefaultRouter()
-web_router.register(r'records', web_apis.Records, basename='records')
-web_router.register(r'collections', web_apis.Collections, basename='collections')
+web_router.register(r'collections', api_web.Collections, basename='collections')
+web_router.register(r'records', api_web.Records, basename='records')
 
 urlpatterns = [
     path('maintenance-mode/', include(maintenance_mode_urls)),
-    path('api/', include((router.urls, 'dalme_app'), namespace='api_endpoint')),
-    path('web-api/', include(web_router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('accounts/login/', views.DalmeLogin.as_view(), name='login'),
-    path('accounts/', include(('django.contrib.auth.urls', 'dalme_app'), namespace='dalme_auth')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('api/', include((router.urls, 'dalme_app'), namespace='api_endpoint')),
+    path('api-web/', include(web_router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('idp/', include('djangosaml2idp.urls', namespace='identity_provider')),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('django_admin/', admin.site.urls),
     re_path(r'^\.well-known/acme-challenge/DWY9GSDZjOsijpklS3RIAuBvZt2PThO7ameePcaIHm8/', lambda request: HttpResponse('DWY9GSDZjOsijpklS3RIAuBvZt2PThO7ameePcaIHm8.LbUmj5n5DqTPM7bapjsa-DennAErlpafYkGP-9eZzzo'), name='hello_world'),
+    path('admin/', admin.site.urls),
+    path('agents/', views.AgentList.as_view(), name='agent_list'),
+    path('async-tasks/', views.AsyncTaskList.as_view(), name='async_task_list'),
+    path('countries/', views.CountryList.as_view(), name='country_list'),
+    path('download/<path:path>/', views.DownloadAttachment, name='download_attachment'),
+    path('models/<model>/', views.ModelLists.as_view(), name='model_lists'),
     path('health/', views.HealthCheck, name='health'),
+    path('images/', views.ImageList.as_view(), name='image_list'),
+    path('images/<slug:pk>/', views.ImageDetail.as_view(), name='image_detail'),
+    path('languages/', views.LanguageList.as_view(), name='language_list'),
+    path('locales/', views.LocaleList.as_view(), name='locale_list'),
+    re_path(r'^pages/(?P<pk>[a-zA-Z0-9-]+)/manifest', views.PageManifest, name='page_manifest'),
+    path('rights/', views.RightsList.as_view(), name='rights_list'),
+    path('rights/<slug:pk>/', views.RightsDetail.as_view(), name='rights_detail'),
     path('search/', views.DefaultSearch.as_view(), name='search'),
-    path('su/', views.SessionUpdate, name='session_update'),
-    path('scripts/', views.Scripts.as_view(), name='scripts'),
     path('sets/', views.SetList.as_view(), name='set_list'),
     path('sets/<slug:pk>/', views.SetsDetail.as_view(), name='set_detail'),
     path('sets/go/<slug:pk>/', views.SetsRedirect.as_view(), name='sets_redirect'),
-    path('models/<model>/', views.ModelLists.as_view(), name='model_lists'),
-    path('languages/', views.LanguageList.as_view(), name='language_list'),
-    path('async_tasks/', views.AsyncTaskList.as_view(), name='async_task_list'),
-    path('countries/', views.CountryList.as_view(), name='country_list'),
-    path('locales/', views.LocaleList.as_view(), name='locale_list'),
-    path('rights/', views.RightsList.as_view(), name='rights_list'),
-    path('rights/<slug:pk>/', views.RightsDetail.as_view(), name='rights_detail'),
-    path('download/<path:path>/', views.DownloadAttachment, name='download_attachment'),
-    re_path(r'^sources/(?P<pk>[a-zA-Z0-9-]+)/manifest', views.SourceManifest, name='source_manifest'),
-    re_path(r'^pages/(?P<pk>[a-zA-Z0-9-]+)/manifest', views.PageManifest, name='page_manifest'),
     path('sources/', views.SourceList.as_view(), name='source_list'),
     path('sources/<slug:pk>/', views.SourceDetail.as_view(), name='source_detail'),
-    path('images/', views.ImageList.as_view(), name='image_list'),
-    path('images/<slug:pk>/', views.ImageDetail.as_view(), name='image_detail'),
-    path('users/', views.UserList.as_view(), name='user_list'),
-    path('users/<username>/', views.UserDetail.as_view(), name='user_detail'),
+    re_path(r'^sources/(?P<pk>[a-zA-Z0-9-]+)/manifest', views.SourceManifest, name='source_manifest'),
+    path('su/', views.SessionUpdate, name='session_update'),
     path('tasks/', views.TasksList.as_view(), name='task_list'),
     path('tasks/<slug:pk>/', views.TasksDetail.as_view(), name='task_detail'),
     path('tickets/', views.TicketList.as_view(), name='ticket_list'),
     path('tickets/<slug:pk>/', views.TicketDetail.as_view(), name='ticket_detail'),
+    path('tools/scripts/', views.Scripts.as_view(), name='scripts'),
+    path('tools/config-editor/', views.ConfigEditor.as_view(), name='config_editor'),
+    path('tools/settings/', views.Settings.as_view(), name='settings'),
+    path('users/', views.UserList.as_view(), name='user_list'),
+    path('users/<username>/', views.UserDetail.as_view(), name='user_detail'),
     path('', views.Index.as_view(), name='dashboard'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
