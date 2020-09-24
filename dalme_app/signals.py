d14209884efa4bd2636a85a4c3e80884bdc5affe
Gabe Pizzorno <pizzorno@fas.harvard.edu>
2020-09-24 11:44:14 -0400

    moved signals from models to separate file

---
+++ dalme_app/signals.py
@@ -0,0 +1,51 @@
+from django.dispatch import receiver
+from django.utils import timezone
+from django.db import models
+from dalme_app.models._templates import get_current_user
+from dalme_app.models import rs_resource, Workflow, Work_log, Page, Source, Transcription
+
+
+@receiver(models.signals.post_save, sender=Page)
+def update_folio(sender, instance, created, **kwargs):
+    if created:
+        rs_image = rs_resource.objects.get(ref=instance.dam_id)
+        rs_image.field79 = instance.name
+        rs_image.save()
+
+
+@receiver(models.signals.post_save, sender=Source)
+def update_workflow(sender, instance, created, **kwargs):
+    if instance.has_inventory:
+        if created:
+            wf_object = Workflow.objects.create(source=instance, last_modified=instance.modification_timestamp)
+            Work_log.objects.create(source=wf_object, event='Source created', timestamp=wf_object.last_modified)
+        else:
+            wf_object = Workflow.objects.get_or_create(source=instance, defaults={
+                'last_modified': timezone.now(),
+                'last_user': get_current_user()
+            })
+            if type(wf_object) is tuple:
+                wf_object[0].last_modified = timezone.now()
+                wf_object[0].last_user = get_current_user()
+                wf_object[0].save()
+
+
+@receiver(models.signals.pre_delete, sender=Source)
+def delete_source_dependencies(sender, instance, **kwargs):
+    if instance.pages:
+        for page in instance.pages.all():
+            if page.sources:
+                for sp in page.sources.all():
+                    if sp.transcription:
+                        sp.transcription.delete()
+            page.delete()
+
+
+@receiver(models.signals.post_save, sender=Transcription)
+def update_source_modification(sender, instance, created, **kwargs):
+    if instance.source_pages.all().exists():
+        source_id = instance.source_pages.all().first().source.id
+        source = Source.objects.get(pk=source_id)
+        source.modification_timestamp = timezone.now()
+        source.modification_user = get_current_user()
+        source.save()
