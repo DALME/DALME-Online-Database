1ee9e8852e916e72509f6b821868b901c614f72e
Gabe Pizzorno <pizzorno@fas.harvard.edu>
2020-09-24 14:35:21 -0400

    generalized corpus filter module into ownership filter module

---
+++ dalme_app/static/js/dalme_helpers/ownership_filter_module.js
@@ -0,0 +1,25 @@
+function ownership_filter_module_load() {
+
+}
+
+function ownership_filter_module_init() {
+  var container = $('<div class="dt-extra-buttons btn-group mr-1" id="ownership_filter"></div>');
+  $(container).append('<button class="btn buttons-collection active" data-of-mode="own" title="Show own records" data-toggle="tooltip">\
+  <i class="fas fa-user fa-sm"></i></button>');
+  $(container).append('<button class="btn buttons-collection" data-of-mode="team" title="Show team records" data-toggle="tooltip">\
+  <i class="fas fa-user-friends fa-sm"></i></button>');
+  $(container).append('<button class="btn buttons-collection" data-of-mode="all" title="Show all non-private records" data-toggle="tooltip">\
+  <i class="fas fa-users fa-sm"></i></button>');
+  container.insertBefore('#dataTables-list_filter');
+  $(container).find('[data-toggle="tooltip"]').tooltip({container: 'body', trigger: 'hover'});
+  $(container).on('click.dalme', 'button', function() {
+      if ($(this).hasClass('active')) {
+        filter_set({'mode': ''}, options=['clear']);
+        $(this).removeClass('active');
+      } else {
+        $(container).find('.active').removeClass('active');
+        filter_set({'mode': $(this).data('of-mode')});
+        $(this).addClass('active');
+      }
+  });
+}
