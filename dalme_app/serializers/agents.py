c689c8eb766ef2428fd42dcfe589eab914a00dea
Gabe Pizzorno <pizzorno@fas.harvard.edu>
2020-10-02 14:10:08 -0400

    fixes to agents serializer

--- dalme_app/serializers/agents.py
+++ dalme_app/serializers/agents.py
@@ -5,11 +5,15 @@ from dalme_app.serializers.users import UserSerializer
 
 
 class AgentSerializer(DynamicSerializer):
-    user = UserSerializer(fields=['full_name', 'username', 'id'])
+    user = UserSerializer(fields=['full_name', 'username', 'id'], required=False)
 
     class Meta:
         model = Agent
         fields = ('id', 'type', 'standard_name', 'notes', 'user')
+        extra_kwargs = {
+            'notes': {'required': False},
+            'user': {'required': False},
+        }
 
     def to_representation(self, instance):
         ret = super().to_representation(instance)
@@ -18,3 +22,9 @@ class AgentSerializer(DynamicSerializer):
             'name': instance.get_type_display()
         }
         return ret
+
+    def to_internal_value(self, data):
+        if data.get('type', {}).get('id') is not None:
+            data['type'] = data['type']['id']
+
+        return super().to_internal_value(data)
