b385aa1f36a85daaaef5b6a399c46e2912dd80dd
Gabe Pizzorno <pizzorno@fas.harvard.edu>
2020-11-02 15:34:06 -0500

    fixes to publications page (expanded document block + changed html and css)

--- dalme_public/blocks.py
+++ dalme_public/blocks.py
@@ -10,8 +10,17 @@ class CarouselBlock(blocks.ListBlock):
 
 
 class DocumentBlock(blocks.StructBlock):
+    type = blocks.ChoiceBlock(
+        choices=[
+            ('document', 'Document'),
+            ('publication', 'Publication'),
+            ('talk', 'Talk')
+        ],
+    )
     title = blocks.CharBlock()
-    abstract = blocks.CharBlock()
+    abstract = blocks.CharBlock(required=False)
+    author = blocks.CharBlock()
+    detail = blocks.CharBlock(required=False)
     version = blocks.FloatBlock(required=False)
     document = DocumentChooserBlock(required=False)
     url = blocks.URLBlock(required=False)
