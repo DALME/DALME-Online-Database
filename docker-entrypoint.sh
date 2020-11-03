803f72ffb30f3a088c37e4bca5e2f8b151574f22
Patrick Altman <paltman@gmail.com>
2020-11-03 16:59:12 -0600

    Dockerize DALME
    This handles running mod_wsgi-express so that that xip.io hosts work

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -22,5 +22,27 @@ echo "DB is up..."
 echo "Running Migrations..."
 python manage.py migrate
 
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+echo "Updating compress manifest..."
+python manage.py compress
+
 echo "Starting runserver..."
-python manage.py runserver 0.0.0.0:8000
+mod_wsgi-express start-server \
+    --url-alias /static www/static \
+    --application-type module dalme.wsgi \
+    --log-to-terminal \
+    --log-level info \
+    --startup-log \
+    --https-port 8443 \
+    --https-only \
+    --server-name db.127.0.0.1.xip.io \
+    --server-alias public.127.0.0.1.xip.io \
+    --server-alias data.127.0.0.1.xip.io \
+    --server-alias purl.127.0.0.1.xip.io \
+    --ssl-certificate-file dev-localhost.cert \
+    --ssl-certificate-key-file dev-localhost.key \
+    --reload-on-changes \
+    --user whiskey \
+    --group root
