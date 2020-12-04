dfd437aa7ebee14ee3100c1a7aa1ee194ba793b5
Gabe Pizzorno <pizzorno@fas.harvard.edu>
2020-12-04 09:33:49 -0500

    trying to work out a good scale for dataset sizes

--- dalme_api/api/datasets.py
+++ dalme_api/api/datasets.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from dalme_api.access_policies import PublicAccessPolicy
 from dalme_app.models import Attribute, LocaleReference
+import math
 
 
 class Datasets(viewsets.GenericViewSet):
@@ -25,20 +26,35 @@ class Datasets(viewsets.GenericViewSet):
 
 def explore_map():
     source_locales = Attribute.objects.filter(attribute_type=36, sources__workflow__is_public=True)
-    result = {}
+    records = {}
     for i in source_locales:
         locale = LocaleReference.objects.filter(id=int(i.value_JSON['id']))
         if locale.exists():
             place = locale.first()
-            if place.name in result:
-                result[place.name]['count'] = result[place.name]['count'] + 1
+            if place.name in records:
+                records[place.name]['count'] += 1
+                records[place.name]['records'] += 1
             else:
-                result[place.name] = {
+                records[place.name] = {
                     'count': 1,
+                    'records': 1,
                     'administrative_region': place.administrative_region,
                     'country': place.country.name,
                     'latitude': place.latitude,
                     'longitude': place.longitude
                 }
 
-    return result
+    # values = [v['count'] for k, v in records.items()]
+    # min_value = min(values)
+    # max_value = max(values)
+    # graph_min = 5
+    # graph_max = 20
+
+    for loc, data in records.items():
+        # https://www.tandfonline.com/doi/pdf/10.1080/02786829608965365
+        # count = data['count'] + (1 - data['count'] / 2) ** 2 if data['count'] < 5 else data['count']
+        # visual range from 5px to 17px
+        data['count'] = math.log(data['count']) * 2
+        # data['count'] = (((data['count'] - min_value) * (graph_max - graph_min)) / (max_value - min_value)) + graph_min
+
+    return records
