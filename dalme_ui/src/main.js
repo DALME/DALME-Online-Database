5be7500f730988955025d03afbf8bf5996b4c840
jhrr <jhrr@users.noreply.github.com>
2021-04-16 20:32:34 +0200

    Mount element-plus in the app

--- dalme_ui/src/main.js
+++ dalme_ui/src/main.js
@@ -1,4 +1,9 @@
+import ElementPlus from "element-plus";
+import "element-plus/lib/theme-chalk/index.css";
 import { createApp } from "vue";
+
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+app.use(ElementPlus);
+app.mount("#app");
