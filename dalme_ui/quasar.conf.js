81a911a6f543b677dd7db51f5c54962ae720f95e
jhrr <jhrr@users.noreply.github.com>
2021-05-27 13:58:02 +0200

    Configure quasar history mode and public path

--- dalme_ui/quasar.conf.js
+++ dalme_ui/quasar.conf.js
@@ -46,7 +46,8 @@ module.exports = configure(function (ctx) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
+      vueRouterMode: 'history', // available values: 'hash', 'history'
+      publicPath: '/ui',
 
       // transpile: false,
 
