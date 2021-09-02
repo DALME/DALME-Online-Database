e7ca8ab6a61bcc1297efb9326bb8eba85a2c4d96
Gabe Pizzorno <pizzorno@fas.harvard.edu>
2021-09-02 13:56:55 -0400

    redesigned recent features widget to use calendar format

--- dalme_public/templatetags/dalme_public_tags.py
+++ dalme_public/templatetags/dalme_public_tags.py
@@ -229,29 +229,61 @@ def get_recent_features(context):
     feature_type = context.get('feature_type')
     if feature_type == 'Essay':
         title = 'Mini Essays'
-        objs = Essay.objects.live().specific().order_by('last_published_at')[:3]
+        objs = Essay.objects.live().specific().order_by('-last_published_at')[:24]
     elif feature_type == 'Inventory':
         title = 'Inventories'
-        objs = FeaturedInventory.objects.live().specific().order_by('last_published_at')[:3]
+        objs = FeaturedInventory.objects.live().specific().order_by('-last_published_at')[:24]
     elif feature_type == 'Object':
         title = 'Objects'
-        objs = FeaturedObject.objects.live().specific().order_by('last_published_at')[:3]
+        objs = FeaturedObject.objects.live().specific().order_by('-last_published_at')[:24]
     else:
         return None
 
     results = {}
-    for obj in objs:
-        year = obj.last_published_at.strftime('%y')
-        if year not in results:
-            results[year] = [{
+    year_control = None
+    year_set = []
+    m_idx = None
+    months = [month_name[i][:3] for i in range(1, 13)]
+
+    for i, obj in enumerate(objs):
+        year = obj.last_published_at.strftime('%Y')
+        month = month_name[obj.last_published_at.month][:3]
+
+        if i == 0:
+            year_control = year
+            m_idx = months.index(month) - 1
+            year_set.append({
                 'url': obj.url,
-                'month': month_name[obj.last_published_at.month]
-            }]
-        else:
-            results[year].append({
-                'url': obj.url,
-                'month': month_name[obj.last_published_at.month]
+                'month': month
             })
+            continue
+
+        if year == year_control:
+            if month != months[m_idx]:
+                while month != months[m_idx]:
+                    year_set.append({
+                        'url': None,
+                        'month': months[m_idx]
+                    })
+                    m_idx -= 1
+
+            year_set.append({
+                'url': obj.url,
+                'month': month
+            })
+            m_idx -= 1
+
+        else:
+            results[year_control] = year_set
+            year_control = year
+            year_set = [{
+                'url': obj.url,
+                'month': month
+            }]
+            m_idx = months.index(month) - 1
+
+        results[year_control] = year_set
+
     if results:
         return {
             'title': title,
