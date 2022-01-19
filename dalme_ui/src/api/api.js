402d95114de9d4f4e69a851a7d265d2bafabf343
jhrr <jhrr@users.noreply.github.com>
2022-01-19 22:25:35 +0000

    Prototype a fetch API state machine

--- dalme_ui/src/api/api.js
+++ dalme_ui/src/api/api.js
@@ -1,6 +1,101 @@
 import { ref } from "vue";
+import { assign, createMachine } from "xstate";
 
 import { fetcher } from "./config";
+
+const fetchMachine = createMachine({
+  id: fetcher,
+  initial: "loading",
+  context: {
+    request: null,
+    data: null,
+    status: null,
+    error: null,
+    apiError: null,
+    redirected: null,
+    retries: 0,
+    maxRetries: 3,
+  },
+  states: {
+    loading: {
+      invoke: {
+        src: "fetch",
+        onDone: {
+          target: "success",
+          actions: "setData",
+        },
+        onError: {
+          target: "failure",
+          actions: "setError",
+        },
+      },
+    },
+    failure: {
+      on: {
+        RETRY: {
+          always: [
+            { target: "loading", cond: "canRetry" },
+            { target: "fatal" },
+          ],
+          actions: "incrementRetries",
+        },
+      },
+    },
+    success: {
+      target: "final",
+    },
+    fatal: {
+      target: "final",
+    },
+    reauthenticate: {
+      target: "final",
+    },
+  },
+  actions: {
+    incrementRetries: assign({
+      retries: (context) => context.retries + 1,
+    }),
+    setData: assign({
+      data: (_, event) => event.data,
+      status: (_, event) => event.status,
+      redirected: (_, event) => event.redirected,
+    }),
+    setError: assign({
+      error: (_, event) => event.data,
+      apiError: (_, event) => event.error,
+      status: (_, event) => event.status,
+      redirected: (_, event) => event.redirected,
+    }),
+  },
+  guards: {
+    canRetry: ({ retries, maxRetries }) => retries < maxRetries,
+    // TODO: Need to integrate reauth logic here.
+  },
+  services: {
+    fetch: (context) => {
+      return fetch(context.request, { credentials: "include" }).then(
+        (response) => response.json(),
+      );
+    },
+  },
+});
+
+// const fetchAPI = (request) => {
+//   const machine = useMachine(fetchMachine, { context: { request } });
+//   const data = computed(() => machine.value.context.data);
+//   const error = computed(() => machine.value.context.error);
+//   const apiError = computed(() => machine.value.context.apiError);
+//   const loading = computed(
+//     () => !["success", "fatal"].some(machine.value.matches),
+//   );
+//   return {
+//     apiError,
+//     data,
+//     error,
+//     loading,
+//     machine,
+//   };
+// };
 
 const API = (context = null) => {
   const loading = ref(false);
