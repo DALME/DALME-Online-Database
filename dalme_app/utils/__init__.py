640bc3d53c4a811dbe637c048f6046f29e771bb1
Gabe Pizzorno <pizzorno@fas.harvard.edu>
2022-08-24 16:42:30 -0400

    removed random bits from DALME web app no longer needed in SPA UI

--- dalme_app/utils/__init__.py
+++ dalme_app/utils/__init__.py
@@ -2,7 +2,6 @@ from .dalme_saml_processor import SAMLProcessor # NOQA
 from .database_router import ModelDatabaseRouter # NOQA
 from .date_time_helpers import FormatDalmeDate, round_timesince # NOQA
 from .domain_middleware import SubdomainRedirectMiddleware # NOQA
-from .menu_compiler import DALMEMenus # NOQA
 from .offline_context_generator import offline_context_generator # NOQA
 from .paginated_formsets import formset_factory # NOQA
 from .search import Search, SearchContext # NOQA
