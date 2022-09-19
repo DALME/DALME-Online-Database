ae53d4400599ea5edad75a3e7a71c1a443b2ef6e
Gabe Pizzorno <pizzorno@fas.harvard.edu>
2022-09-19 16:59:56 -0400

    added utility components to render boolean values and workflow tags

---
+++ dalme_ui/src/components/utils/BooleanIcon.vue
@@ -0,0 +1,56 @@
+<template>
+  <div v-if="onlyTrue">
+    <q-icon
+      v-if="value"
+      :name="trueIcon"
+      :color="onlyTrueGreen ? 'green-7' : 'red-6'"
+      :size="size"
+    />
+  </div>
+  <div v-else>
+    <q-icon
+      :name="value ? trueIcon : falseIcon"
+      :color="value ? 'green-7' : 'red-6'"
+      :size="size"
+    />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "BooleanIcon",
+  props: {
+    onlyTrue: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    onlyTrueGreen: {
+      type: Boolean,
+      required: false,
+      default: true,
+    },
+    trueIcon: {
+      type: String,
+      required: false,
+      default: "check_box",
+    },
+    falseIcon: {
+      type: String,
+      required: false,
+      default: "disabled_by_default",
+    },
+    value: {
+      type: Boolean,
+      required: true,
+    },
+    size: {
+      type: String,
+      required: false,
+      default: "xs",
+    },
+  },
+});
+</script>
--- dalme_ui/src/components/utils/index.js
+++ dalme_ui/src/components/utils/index.js
@@ -1,4 +1,6 @@
+export { default as BooleanIcon } from "./BooleanIcon.vue";
 export { default as OpaqueSpinner } from "./OpaqueSpinner.vue";
 export { default as Spinner } from "./Spinner.vue";
 export { default as SuspenseWithError } from "./SuspenseWithError.vue";
 export { default as Tooltip } from "./Tooltip.vue";
+export { default as WorkflowTag } from "./WorkflowTag.vue";
