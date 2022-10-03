1504db43bdb535145a76a7db2373d6ebe1f9fd40
Gabe Pizzorno <pizzorno@fas.harvard.edu>
2022-10-03 16:38:26 -0400

    created WorkflowManager component

---
+++ dalme_ui/src/components/workflow-manager/WorkflowManager.vue
@@ -0,0 +1,77 @@
+<template>
+  <q-btn-dropdown
+    dense
+    outline
+    :icon="icon"
+    :color="colours.colour"
+    :text-color="colours.text"
+    :class="`workflow-button bg-${colours.colour}`"
+    :label="data.status.text"
+  >
+    <q-card>
+      <q-card-section horizontal>
+        <q-card-section>somthing</q-card-section>
+        <q-card-section>
+          <q-list dense separator class="text-grey-9">
+            <q-item clickable v-close-popup>
+              <q-item-section>
+                <q-item-label>Mark stage as done/Start stage</q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-item clickable v-close-popup>
+              <q-item-section>
+                <q-item-label>Flag for assistance</q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-item clickable v-close-popup>
+              <q-item-section>
+                <q-item-label>Place under assessment</q-item-label>
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-card-section>
+      </q-card-section>
+    </q-card>
+  </q-btn-dropdown>
+</template>
+
+<script>
+import { computed, defineComponent } from "vue";
+import { useConstantStore } from "@/stores/constants";
+
+export default defineComponent({
+  name: "WorkflowManager",
+  props: {
+    data: {
+      type: Object,
+      required: true,
+    },
+  },
+  setup(props) {
+    const $constantStore = useConstantStore();
+    const colours = computed(
+      () => $constantStore.workflowTagColours[props.data.status.tag],
+    );
+    const icon = computed(
+      () => $constantStore.workflowIconbyStage[props.data.stage],
+    );
+
+    return {
+      colours,
+      icon,
+    };
+  },
+});
+</script>
+
+<style lang="scss">
+.workflow-button {
+  font-weight: 600;
+  font-size: 12px;
+}
+.workflow-button i:first-of-type {
+  font-size: 18px;
+  font-weight: 200;
+  margin-left: 5px;
+}
+</style>
---
+++ dalme_ui/src/components/workflow-manager/index.js
@@ -0,0 +1,1 @@
+export { default } from "./WorkflowManager.vue";
