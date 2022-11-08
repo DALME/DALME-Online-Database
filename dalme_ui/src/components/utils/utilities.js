92e85b13a98be387fa9e7940d2ac446a91400d78
Gabe Pizzorno <pizzorno@fas.harvard.edu>
2022-11-08 16:16:46 -0500

    added utility to format UUIDs

--- dalme_ui/src/components/utils/utilities.js
+++ dalme_ui/src/components/utils/utilities.js
@@ -1,3 +1,6 @@
 import { isEmpty, isNil } from "ramda";
 
 export const notNully = (value) => !isNil(value) && !isEmpty(value);
+
+export const formatUuid = (value) =>
+  `${value.substr(0, 8)}-${value.substr(8, 4)}-${value.substr(12, 4)}-${value.substr(16, 4)}-${value.substr(20)}`; // eslint-disable-line
