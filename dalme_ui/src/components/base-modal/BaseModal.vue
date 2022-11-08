82955c6e28c061c3707d755645cb520b25b8655b
Gabe Pizzorno <pizzorno@fas.harvard.edu>
2022-11-08 15:52:32 -0500

    re-styled editing modals

--- dalme_ui/src/components/base-modal/BaseModal.vue
+++ dalme_ui/src/components/base-modal/BaseModal.vue
@@ -1,98 +1,44 @@
 <template>
   <teleport to="body">
     <div
-      class="modal-container"
+      :class="`${containerClasses} ${zClass}`"
       ref="el"
-      :class="isFocus ? 'z-max' : 'z-top'"
       :style="`touch-action:none;${dragging}`"
       v-show="visible"
       @click.stop="handleFocus"
       v-ripple:blue-1
     >
-      <q-card
-        class="modal-card q-pt-none q-pb-md"
-        :class="{
-          focussed: cuid === focus,
-          pulse: valid && mouseoverSubmit && cuid === focus,
-        }"
-      >
-        <q-card-section
-          class="header bg-grey-2 q-py-sm q-px-md row no-wrap flex-center"
-          :class="isFocus ? 'z-max' : 'z-top'"
-        >
-          <span class="text-caption text-grey-8">
-            <code>{{ cuid }}</code>
-          </span>
-
-          <q-btn
-            round
-            class="q-ml-auto"
-            color="white"
-            text-color="black"
-            icon="minimize"
-            size="xs"
-            @click.stop="handleMinimize"
-          >
-            <Tooltip> Minimize </Tooltip>
-          </q-btn>
-
-          <q-btn
-            round
-            class="q-ml-xs"
-            color="deep-orange"
-            icon="close"
-            size="xs"
-            @click.stop="confirm = true"
-          >
-            <Tooltip> Close </Tooltip>
-          </q-btn>
+      <q-card borderless class="modal-card">
+        <q-card-section :class="`header ${zClass}`">
+          <div class="title">{{ modalTitle }}</div>
+          <div class="header-button-group">
+            <q-btn icon="o_visibility_off" @click.stop="handleMinimize">
+              <Tooltip> Minimize </Tooltip>
+            </q-btn>
+            <q-btn icon="close" @click.stop="confirm">
+              <Tooltip> Close </Tooltip>
+            </q-btn>
+          </div>
         </q-card-section>
 
-        <div class="text-h5 text-capitalize text-bold q-pt-sm q-px-md">
+        <!-- <div class="text-h5 text-capitalize modal-title-slot">
           <slot name="title"></slot>
-        </div>
+        </div> -->
 
         <slot name="content"></slot>
       </q-card>
     </div>
-
-    <q-dialog v-model="confirm" persistent class="z-max">
-      <q-card>
-        <q-card-section class="row items-center">
-          <q-avatar icon="close" color="red" text-color="white" size="sm" />
-          <span class="q-ml-sm">Close modal?</span>
-        </q-card-section>
-
-        <q-card-actions align="right">
-          <q-btn
-            unelevated
-            no-caps
-            label="Cancel"
-            color="primary"
-            v-close-popup
-          />
-          <q-btn
-            unelevated
-            no-caps
-            label="Close"
-            color="primary"
-            v-close-popup
-            @click.stop="handleClose"
-          />
-        </q-card-actions>
-      </q-card>
-    </q-dialog>
-
     <q-inner-loading :showing="submitting" />
   </teleport>
 </template>
 
 <script>
+import { format, useQuasar } from "quasar";
 import { computed, defineAsyncComponent, defineComponent, ref } from "vue";
 import { useDraggable, useStorage } from "@vueuse/core";
 import { useActor, useSelector } from "@xstate/vue";
-
 import { useEditing } from "@/use";
+import { Dialog } from "@/components";
 
 export default defineComponent({
   name: "BaseModal",
@@ -116,6 +62,8 @@ export default defineComponent({
     ),
   },
   setup(props) {
+    const $q = useQuasar();
+    const { capitalize } = format;
     const {
       focus,
       modals,
@@ -124,15 +72,27 @@ export default defineComponent({
       submitting,
       machine: { send },
     } = useEditing();
-
     const { actor } = modals.value[props.cuid];
     const { send: actorSend } = useActor(actor);
     const visible = useSelector(actor, (state) => state.context.visible);
     const valid = useSelector(actor, (state) => state.context.validated);
-
-    const confirm = ref(false);
     const el = ref(null);
-    const isFocus = computed(() => focus.value === props.cuid);
+    const ctx = actor.machine.config.context;
+    const modalTitle = computed(() =>
+      ctx.mode === "create"
+        ? `New ${ctx.kind}`
+        : `${capitalize(ctx.mode)} ${ctx.initialData.shortName}`,
+    );
+    const zClass = computed(() =>
+      props.cuid === focus.value ? "z-max" : "z-top",
+    );
+    const containerClasses = computed(() => {
+      let classes = ["modal-container"];
+      if (props.cuid === focus.value) classes.push("focussed");
+      if (valid.value && mouseoverSubmit.value && props.cuid === focus.value)
+        classes.push("pulse");
+      return classes.join(" ");
+    });
     const positionKey = `form-position:${props.cuid}`;
     const positionValue = useStorage(
       positionKey,
@@ -150,56 +110,136 @@ export default defineComponent({
 
     const handleFocus = () => send("SET_FOCUS", { value: props.cuid });
     const handleMinimize = () => actorSend("HIDE");
-    const handleClose = () => {
-      send("DESTROY_MODAL", { cuid: props.cuid });
+    const confirm = () => {
+      let message = `Do you want to close the window <br />\
+        <span class="text-weight-medium">${modalTitle.value}</span>?`;
+      if (valid.value)
+        /* eslint-disable */
+        message = message + '<br /> <span class="text-red-10 text-weight-medium">\
+          Your changes will be lost.</span>';
+        /* eslint-enable */
+      $q.dialog({
+        component: Dialog,
+        componentProps: {
+          isPersistent: true,
+          title: "Close window",
+          closeIcon: false,
+          message: message,
+          icon: "disabled_by_default",
+          okayButtonLabel: "Close",
+          classes: "z-max",
+          colourScheme: valid.value ? "warning" : "edit-mode",
+        },
+      }).onOk(() => {
+        send("DESTROY_MODAL", { cuid: props.cuid });
+      });
     };
 
     recenterWatcher(props.cuid, x, y);
 
     return {
       confirm,
+      containerClasses,
       dragging,
       el,
       focus,
-      handleClose,
       handleFocus,
       handleMinimize,
-      isFocus,
+      modalTitle,
       mouseoverSubmit,
       submitting,
       valid,
       visible,
+      zClass,
     };
   },
 });
 </script>
 
 <style lang="scss" scoped>
-.focussed {
-  border-left: 4px solid green;
-  transition: border 0.05s linear;
-}
-.header {
-  border-bottom: 1px solid #e0e0e0;
-  opacity: 1;
-  position: sticky;
-  top: 0;
-}
-.pulse {
-  border-left: 8px solid red;
-  transition: border 0.5s linear;
-}
 .modal-container {
-  box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
   min-width: 45rem;
   max-width: 50rem;
   position: fixed;
+  border-radius: 4px;
   will-change: auto !important;
+  box-shadow: rgba(140, 149, 159, 0.2) 0px 8px 24px 0px;
 }
 .modal-card {
-  border-radius: 0 !important;
   max-height: calc(100vh - 8rem);
   overflow-y: auto;
   scroll-snap-type: y proximity;
+  padding-bottom: 10px;
+  border-radius: 4px;
+  border-width: 1px;
+  border-style: solid;
+  border-color: #d1d1d1;
+}
+.modal-card > div:first-child {
+  border-top-left-radius: initial;
+  border-top-right-radius: initial;
+}
+.focussed .modal-card {
+  border-color: #c87001;
+}
+.pulse .modal-card {
+  border-color: #c3747c;
+}
+.header {
+  height: 32px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  position: sticky;
+  top: 0;
+  padding: 0 12px;
+  opacity: 1;
+  color: #bdbdbd;
+  background-color: #f5f5f5;
+  border-color: inherit;
+  border-bottom-width: 1px;
+  border-bottom-style: solid;
+}
+.focussed .header {
+  color: #a85e00;
+  background-color: #ffca82;
+}
+.pulse .header {
+  color: #ac0300;
+  background-color: #ffccd4;
+}
+.title {
+  font-size: 12px;
+  font-weight: 600;
+}
+.header-button-group {
+  display: flex;
+  border-color: inherit;
+  border-right-width: 1px;
+  border-right-style: solid;
+  border-left-width: 1px;
+  border-left-style: solid;
+}
+.header-button-group button {
+  font-size: 11px;
+  padding: 4px;
+  height: 30px;
+  width: 30px;
+  border-color: inherit;
+  border-radius: 0;
+  border-right-width: 1px;
+  border-right-style: dotted;
+}
+.header-button-group button::before {
+  box-shadow: none;
+}
+.header-button-group button:last-of-type {
+  border-right: none;
+}
+.modal-title-slot {
+  background-color: #f5f5f5;
+  padding: 10px 15px;
+  border-bottom: 1px solid #d1d1d1;
 }
 </style>
