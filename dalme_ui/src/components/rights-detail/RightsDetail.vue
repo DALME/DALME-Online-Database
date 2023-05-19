d94cb450609e91d20a69189c7af392b7d461b3f8
Gabe Pizzorno <pizzorno@fas.harvard.edu>
2023-05-19 09:55:31 -0400

    added RightsDetail component

---
+++ dalme_ui/src/components/rights-detail/RightsDetail.vue
@@ -0,0 +1,11 @@
+<template>
+  <div />
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "RightsDetail",
+});
+</script>
---
+++ dalme_ui/src/components/rights-detail/index.js
@@ -0,0 +1,1 @@
+export { default } from "./RightsDetail.vue";
