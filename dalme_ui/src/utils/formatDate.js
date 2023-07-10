1cefbdf26bcc909f2a7b14c7f77062c3e589f9a8
Gabe Pizzorno <pizzorno@fas.harvard.edu>
2023-07-10 13:36:20 -0400

    slight refactoring of data formatter

--- dalme_ui/src/utils/formatDate.js
+++ dalme_ui/src/utils/formatDate.js
@@ -4,21 +4,27 @@ import { format as timeagoFormat } from "timeago.js";
 export const formatDate = (
   string,
   withTime = true,
+  type = null,
   prefixes = ["on ", ""],
   suffixes = null,
 ) => {
-  let date = null;
   const format = withTime ? "D MMM YYYY @ H:mm" : "D MMM YYYY";
+  let output = null;
   if (qDate.isValid(string)) {
-    if (qDate.getDateDiff(Date.now(), string, "days") > 7) {
-      date = qDate.formatDate(string, format);
-      if (prefixes) date = `${prefixes[0]}${date}`;
-      if (suffixes) date = `${date}${suffixes[0]}`;
+    if (!type) {
+      type = qDate.getDateDiff(Date.now(), string, "days") > 7 ? "d" : "h";
     } else {
-      date = timeagoFormat(string);
-      if (prefixes) date = `${prefixes[1]}${date}`;
-      if (suffixes) date = `${date}${suffixes[1]}`;
+      prefixes = null;
+    }
+    if (type == "d") {
+      output = qDate.formatDate(string, format);
+      if (prefixes) output = `${prefixes[0]}${output}`;
+      if (suffixes) output = `${output}${suffixes[0]}`;
+    } else {
+      output = timeagoFormat(string);
+      if (prefixes) output = `${prefixes[1]}${output}`;
+      if (suffixes) output = `${output}${suffixes[1]}`;
     }
   }
-  return date;
+  return output;
 };
