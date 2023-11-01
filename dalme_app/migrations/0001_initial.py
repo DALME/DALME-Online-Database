db49cdb6beaacafc9fa0388dc6b86f0f47071bcf
jhrr <jhrr@users.noreply.github.com>
2023-11-01 15:05:41 +0000

    Remove custom sql casts from migration

--- dalme_app/migrations/0001_initial.py
+++ dalme_app/migrations/0001_initial.py
@@ -26,17 +26,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        # The following SQL statements are a workaround for an issue filtering
-        # over generic relations in postgres when UUID foreign keys are stored
-        # in text/char fields. This is a known and long-standing bug in Django:
-        # https://code.djangoproject.com/ticket/16055#no2
-        # The problem still exists, despite being ostensibly fixed with a patch
-        # in 2023:
-        # https://github.com/django/django/pull/16632/files#diff-ca59ee7c2039f916cf688bb872c67dd07447d3475b6b6b279092a53dbc0c38ab
-        migrations.RunSQL("CREATE CAST (character varying AS uuid) WITH INOUT AS IMPLICIT;"),
-        migrations.RunSQL("CREATE CAST (uuid AS character varying) WITH INOUT AS IMPLICIT;"),
-        migrations.RunSQL("CREATE CAST (character varying AS integer) WITH INOUT AS IMPLICIT;"),
-        migrations.RunSQL("CREATE CAST (integer AS character varying) WITH INOUT AS IMPLICIT;"),
         migrations.CreateModel(
             name='rs_collection',
             fields=[
