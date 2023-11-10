aa528c45b81d19dde3eb7d1df709335380d6b811
jhrr <jhrr@users.noreply.github.com>
2023-11-10 23:19:14 +0000

    Refactor and test the tenants collectstatic command

--- dalme_app/management/commands/collectstatic_tenants.py
+++ dalme_app/management/commands/collectstatic_tenants.py
@@ -1,8 +1,9 @@
-"""Run s3manifestcollectstatic for all db schemas.
+"""Run collectstatic for all db schemas.
 
 Adapted from: https://github.com/dduong42/s3manifestcollectstatic
 
 """
+import functools
 import json
 import os
 from concurrent.futures import ThreadPoolExecutor
@@ -36,6 +37,101 @@ class Command(BaseCommand):
         """Are we running in dev mode or not."""
         return os.environ['ENV'] in {'development', 'ci'}
 
+    @staticmethod
+    def save_asset(path, tmpdir, tenant):
+        """Upload the file data to the bucket.
+
+        We have to set the tenant context here again as we are running inside
+        the thread pool. If we don't the tenant will always be 'public' as the
+        db connection will not yet have been set within the new thread.
+
+        """
+        path_obj = Path(tmpdir) / path
+        with tenant_context(tenant), path_obj.open('rb') as f:
+            staticfiles_storage.save(path, f)
+        return path
+
+    @staticmethod
+    def parse_manifest(handle):
+        """Read the contents of a static manifest file."""
+        return set(json.load(handle)['paths'].values())
+
+    @staticmethod
+    def collect(tmpdir):
+        """Collect files into a temporary directory and generate a manifest."""
+        command = collectstatic.Command()
+        command.storage = ManifestStaticFilesStorage(location=tmpdir)
+        call_command(command)
+
+    def get_file_data(self, tenant):
+        """Process the files and collect the resulting file data for the run."""
+        with TemporaryDirectory() as tmpdir:
+            self.collect(tmpdir)
+
+            manifest = Path(tmpdir) / MANIFEST_PATH
+            with manifest.open('rb') as f:
+                to_upload = self.parse_manifest(f)
+
+            # Compare the newly generated manifest with the remote manifest (if
+            # it exists) to determine what needs to be sent down the wire.
+            if staticfiles_storage.exists(MANIFEST_PATH):
+                with staticfiles_storage.open(MANIFEST_PATH) as f:
+                    already_uploaded = self.parse_manifest(f)
+                    intersection = to_upload.intersection(already_uploaded)
+
+                    if self.force:
+                        logger.info(
+                            'Forcing the reupload of all staticfiles for tenant: %s',
+                            tenant=tenant.schema_name,
+                        )
+                    else:
+                        to_upload.difference_update(already_uploaded)
+                        logger.info(
+                            '%s files already exist, those uploads will be skipped for tenant: %s',
+                            count=len(intersection),
+                            tenant=tenant.schema_name,
+                        )
+
+            return tmpdir, to_upload
+
+    def collectstatic_s3(self, tenant):
+        """Efficiently upload staticfiles to s3."""
+        with tenant_context(tenant):
+            if self.max_workers is not None and self.max_workers < 1:
+                msg = 'The maximum number of workers must be greater than 0.'
+                raise CommandError(msg)
+
+            tmpdir, to_upload = self.get_file_data(tenant)
+            if (count := len(to_upload)) > 0:
+                logger.info('Uploading %s files for tenant: %s', count=count, tenant=tenant.schema_name)
+
+            with ThreadPoolExecutor(max_workers=self.max_workers) as executor:
+                func = functools.partial(self.save_asset, tenant=tenant, tmpdir=tmpdir)
+                for path in executor.map(func, to_upload):
+                    logger.info('File was uploaded to: %s/%s', tenant=tenant.schema_name, path=path)
+
+            logger.info('Uploading the manifest for tenant: %s', tenant=tenant.schema_name)
+            self.save_asset(MANIFEST_PATH, tmpdir, tenant)
+
+    def collectstatic(self, tenant):
+        """Invoke the normal django collectstatic command."""
+        with tenant_context(tenant):
+            call_command(collectstatic.Command())
+
+    def handle(self, *args, **options):  # noqa: ARG002
+        """Collect application staticfiles per tenant."""
+        self.force = options.get('force', False)
+        self.max_workers = options.get('max_workers')
+
+        command = self.collectstatic if self.is_dev else self.collectstatic_s3
+        connection.set_schema_to_public()
+        logger.info('Collecting staticfiles for tenant: %s', tenant='public')
+        command(connection.tenant)
+
+        for tenant in Tenant.objects.all():
+            logger.info('Collecting staticfiles for tenant: %s', tenant=tenant.schema_name)
+            command(tenant)
+
     def add_arguments(self, parser):
         """Configure accepted managment command arguments."""
         parser.add_argument(
@@ -50,66 +146,3 @@ class Command(BaseCommand):
             type=int,
             help='Max number of workers',
         )
-
-    def collectstatic(self, *args):  # noqa: ARG002
-        """Invoke the normal django collectstatic command."""
-        command = collectstatic.Command()
-        call_command(command)
-
-    def collectstatic_s3(self, tenant, options):
-        """Efficiently upload staticfiles to s3."""
-        with tenant_context(tenant):
-            self.force = options.get('force', False)
-            self.max_workers = options.get('max_workers')
-
-            if self.max_workers is not None and self.max_workers <= 0:
-                msg = 'The maximum number of workers must be greater than 0.'
-                raise CommandError(msg)
-
-            with TemporaryDirectory() as tmpdirname:
-                command = collectstatic.Command()
-                command.storage = ManifestStaticFilesStorage(location=tmpdirname)
-                call_command(command)
-
-                manifest = Path(tmpdirname) / MANIFEST_PATH
-                with manifest.open('rb') as f:
-                    to_upload = set(json.load(f)['paths'].values())
-
-                if staticfiles_storage.exists(MANIFEST_PATH):
-                    with staticfiles_storage.open(MANIFEST_PATH) as f:
-                        already_uploaded = set(json.load(f)['paths'].values())
-                        intersection = to_upload.intersection(already_uploaded)
-                        logger.info('%s files were already uploaded', len(intersection))
-
-                        if self.force:
-                            logger.info('Forcing the reupload of files')
-                        else:
-                            to_upload.difference_update(already_uploaded)
-
-                def _save_asset(path):
-                    path_obj = Path(tmpdirname) / path
-                    # We have to set the tenant context here again for when we
-                    # are running inside the thread pool otherwise the tenant
-                    # will always be 'public'.
-                    with path_obj.open('rb') as f, tenant_context(tenant):
-                        staticfiles_storage.save(path, f)
-                    return path
-
-                logger.info('Start the upload of %s files', len(to_upload))
-                with ThreadPoolExecutor(max_workers=self.max_workers) as executor:
-                    for path in executor.map(_save_asset, to_upload):
-                        logger.info('Path was uploaded: %s', path)
-
-                logger.info('Uploading the manifest')
-                _save_asset(MANIFEST_PATH)
-
-    def handle(self, *args, **options):  # noqa: ARG002
-        """Collect staticfiles."""
-        command = self.collectstatic if self.is_dev else self.collectstatic_s3
-        connection.set_schema_to_public()
-        logger.info('Collecting staticfiles for: public')
-        command(connection.tenant, options)
-
-        for tenant in Tenant.objects.all():
-            logger.info('Collecting staticfiles for: %s', tenant)
-            command(tenant, options)
--- tests/unit/dalme_app/management/commands/test_collectstatic_tenants.py
+++ tests/unit/dalme_app/management/commands/test_collectstatic_tenants.py
@@ -1,1 +1,393 @@
 """Test the dalme_app.management.commands.collectstatic_tenants module."""
+import argparse
+import json
+import os
+from unittest import mock
+
+import pytest
+
+from django.core.management.base import CommandError
+
+from dalme_app.management.commands.collectstatic_tenants import Command as CollectstaticTenants
+from dalme_app.models import Tenant
+
+MANIFEST_DATA = json.dumps(
+    {
+        'paths': {
+            'some/js/file.js': 'some/js/file.034cc7d8a67f.js',
+            'some/css/file.css': 'some/css/file.a2194c262648.css',
+        },
+        'version': '1.1',
+        'hash': '2e773d792c4d',
+    }
+)
+
+
+def test_collectstatic_tenants_args():
+    """Test the argument handler for the command."""
+    parser = argparse.ArgumentParser()
+    CollectstaticTenants().add_arguments(parser)
+
+    assert parser.parse_args(['--force', '--max-workers', '4']) == argparse.Namespace(force=True, max_workers=4)
+
+
+@mock.patch.dict(os.environ, {'ENV': 'development'})
+@mock.patch('dalme_app.management.commands.collectstatic_tenants.connection')
+@mock.patch('dalme_app.management.commands.collectstatic_tenants.call_command')
+@mock.patch('dalme_app.management.commands.collectstatic_tenants.Tenant')
+@mock.patch('dalme_app.management.commands.collectstatic_tenants.logger')
+def test_collectstatic_tenants_development(mock_logger, mock_tenant, mock_call_command, mock_connection):
+    tenant_1 = mock.MagicMock(spec=Tenant)
+    tenant_1.schema_name = 'tenant1'
+    tenant_2 = mock.MagicMock(spec=Tenant)
+    tenant_2.schema_name = 'tenant2'
+    mock_tenant.objects.all.return_value = [tenant_1, tenant_2]
+
+    CollectstaticTenants().handle()
+
+    assert mock_call_command.called
+    assert mock_connection.mock_calls == [
+        mock.call.set_schema_to_public(),
+    ]
+    assert mock_logger.mock_calls == [
+        mock.call.info('Collecting staticfiles for tenant: %s', tenant='public'),
+        mock.call.info('Collecting staticfiles for tenant: %s', tenant='tenant1'),
+        mock.call.info('Collecting staticfiles for tenant: %s', tenant='tenant2'),
+    ]
+
+
+@mock.patch.dict(os.environ, {'ENV': 'production'})
+@mock.patch('dalme_app.management.commands.collectstatic_tenants.connection')
+@mock.patch('dalme_app.management.commands.collectstatic_tenants.call_command')
+@mock.patch('dalme_app.management.commands.collectstatic_tenants.Tenant')
+@mock.patch('dalme_app.management.commands.collectstatic_tenants.logger')
+def test_collectstatic_tenants_insufficient_workers(mock_logger, mock_tenant, mock_call_command, mock_connection):
+    tenant_1 = mock.MagicMock(spec=Tenant)
+    tenant_1.schema_name = 'tenant1'
+    tenant_2 = mock.MagicMock(spec=Tenant)
+    tenant_2.schema_name = 'tenant2'
+    mock_tenant.objects.all.return_value = [tenant_1, tenant_2]
+
+    with pytest.raises(CommandError) as exc:
+        CollectstaticTenants().handle(max_workers=0)
+
+    assert str(exc.value) == 'The maximum number of workers must be greater than 0.'
+
+    assert not mock_call_command.called
+    assert mock_connection.mock_calls == [
+        mock.call.set_schema_to_public(),
+    ]
+    assert mock_logger.mock_calls == [
+        mock.call.info('Collecting staticfiles for tenant: %s', tenant='public'),
+    ]
+
+
+@mock.patch.dict(os.environ, {'ENV': 'production'})
+@mock.patch('dalme_app.management.commands.collectstatic_tenants.staticfiles_storage')
+@mock.patch('dalme_app.management.commands.collectstatic_tenants.call_command')
+@mock.patch('dalme_app.management.commands.collectstatic_tenants.Tenant')
+@mock.patch('dalme_app.management.commands.collectstatic_tenants.logger')
+def test_collectstatic_tenants_does_not_exist(mock_logger, mock_tenant, mock_call_command, mock_storage):
+    tenant_1 = mock.MagicMock(spec=Tenant)
+    tenant_1.schema_name = 'tenant1'
+    tenant_2 = mock.MagicMock(spec=Tenant)
+    tenant_2.schema_name = 'tenant2'
+    mock_tenant.objects.all.return_value = [tenant_1, tenant_2]
+    mock_storage.exists.return_value = False
+
+    with mock.patch(
+        'dalme_app.management.commands.collectstatic_tenants.Path.open', mock.mock_open(read_data=MANIFEST_DATA)
+    ) as mock_open:
+        CollectstaticTenants().handle()
+
+        assert mock_open.mock_calls == [
+            # public
+            mock.call('rb'),
+            mock.call().__enter__(),
+            mock.call().read(),
+            mock.call().__exit__(None, None, None),
+            mock.call('rb'),
+            mock.call().__enter__(),
+            mock.call().__exit__(None, None, None),
+            mock.call('rb'),
+            mock.call().__enter__(),
+            mock.call().__exit__(None, None, None),
+            mock.call('rb'),
+            mock.call().__enter__(),
+            mock.call().__exit__(None, None, None),
+            # tenant1
+            mock.call('rb'),
+            mock.call().__enter__(),
+            mock.call().read(),
+            mock.call().__exit__(None, None, None),
+            mock.call('rb'),
+            mock.call().__enter__(),
+            mock.call().__exit__(None, None, None),
+            mock.call('rb'),
+            mock.call().__enter__(),
+            mock.call().__exit__(None, None, None),
+            mock.call('rb'),
+            mock.call().__enter__(),
+            mock.call().__exit__(None, None, None),
+            # tenant2
+            mock.call('rb'),
+            mock.call().__enter__(),
+            mock.call().read(),
+            mock.call().__exit__(None, None, None),
+            mock.call('rb'),
+            mock.call().__enter__(),
+            mock.call().__exit__(None, None, None),
+            mock.call('rb'),
+            mock.call().__enter__(),
+            mock.call().__exit__(None, None, None),
+            mock.call('rb'),
+            mock.call().__enter__(),
+            mock.call().__exit__(None, None, None),
+        ]
+
+    assert mock_call_command.called
+
+    # The order is non-deterministic in the following data because of the
+    # threading pool so we can just loop instead to account for eveything.
+    for call in [
+        mock.call.exists('staticfiles.json'),
+        mock.call.save('some/css/file.a2194c262648.css', mock_open.return_value),
+        mock.call.save('some/js/file.034cc7d8a67f.js', mock_open.return_value),
+        mock.call.save('staticfiles.json', mock_open.return_value),
+    ]:
+        assert call in mock_storage.mock_calls
+
+    for call in [
+        mock.call.info('Collecting staticfiles for tenant: %s', tenant='public'),
+        mock.call.info('Uploading %s files for tenant: %s', count=2, tenant='public'),
+        mock.call.info('File was uploaded to: %s/%s', tenant='public', path='some/js/file.034cc7d8a67f.js'),
+        mock.call.info('File was uploaded to: %s/%s', tenant='public', path='some/css/file.a2194c262648.css'),
+        mock.call.info('Uploading the manifest for tenant: %s', tenant='public'),
+        mock.call.info('Collecting staticfiles for tenant: %s', tenant='tenant1'),
+        mock.call.info('Uploading %s files for tenant: %s', count=2, tenant='tenant1'),
+        mock.call.info('File was uploaded to: %s/%s', tenant='tenant1', path='some/js/file.034cc7d8a67f.js'),
+        mock.call.info('File was uploaded to: %s/%s', tenant='tenant1', path='some/css/file.a2194c262648.css'),
+        mock.call.info('Uploading the manifest for tenant: %s', tenant='tenant1'),
+        mock.call.info('Collecting staticfiles for tenant: %s', tenant='tenant2'),
+        mock.call.info('Uploading %s files for tenant: %s', count=2, tenant='tenant2'),
+        mock.call.info('File was uploaded to: %s/%s', tenant='tenant2', path='some/js/file.034cc7d8a67f.js'),
+        mock.call.info('File was uploaded to: %s/%s', tenant='tenant2', path='some/css/file.a2194c262648.css'),
+        mock.call.info('Uploading the manifest for tenant: %s', tenant='tenant2'),
+    ]:
+        assert call in mock_logger.mock_calls
+
+
+@mock.patch.dict(os.environ, {'ENV': 'production'})
+@mock.patch('dalme_app.management.commands.collectstatic_tenants.staticfiles_storage')
+@mock.patch('dalme_app.management.commands.collectstatic_tenants.call_command')
+@mock.patch('dalme_app.management.commands.collectstatic_tenants.Tenant')
+@mock.patch('dalme_app.management.commands.collectstatic_tenants.logger')
+def test_collectstatic_tenants_exists(mock_logger, mock_tenant, mock_call_command, mock_storage):
+    tenant_1 = mock.MagicMock(spec=Tenant)
+    tenant_1.schema_name = 'tenant1'
+    tenant_2 = mock.MagicMock(spec=Tenant)
+    tenant_2.schema_name = 'tenant2'
+    mock_tenant.objects.all.return_value = [tenant_1, tenant_2]
+    mock_storage.exists.return_value = True
+
+    with mock.patch(
+        'dalme_app.management.commands.collectstatic_tenants.Path.open', mock.mock_open(read_data=MANIFEST_DATA)
+    ) as mock_open, mock.patch(
+        'dalme_app.management.commands.collectstatic_tenants.staticfiles_storage.open',
+        mock.mock_open(read_data=MANIFEST_DATA),
+    ) as mock_storage_open:
+        CollectstaticTenants().handle()
+
+        assert mock_open.mock_calls == [
+            # public
+            mock.call('rb'),
+            mock.call().__enter__(),
+            mock.call().read(),
+            mock.call().__exit__(None, None, None),
+            mock.call('rb'),
+            mock.call().__enter__(),
+            mock.call().__exit__(None, None, None),
+            # tenant1
+            mock.call('rb'),
+            mock.call().__enter__(),
+            mock.call().read(),
+            mock.call().__exit__(None, None, None),
+            mock.call('rb'),
+            mock.call().__enter__(),
+            mock.call().__exit__(None, None, None),
+            # tenant2
+            mock.call('rb'),
+            mock.call().__enter__(),
+            mock.call().read(),
+            mock.call().__exit__(None, None, None),
+            mock.call('rb'),
+            mock.call().__enter__(),
+            mock.call().__exit__(None, None, None),
+        ]
+        assert mock_storage_open.mock_calls == [
+            # public
+            mock.call('staticfiles.json'),
+            mock.call().__enter__(),
+            mock.call().read(),
+            mock.call().__exit__(None, None, None),
+            # tenant1
+            mock.call('staticfiles.json'),
+            mock.call().__enter__(),
+            mock.call().read(),
+            mock.call().__exit__(None, None, None),
+            # tenant2
+            mock.call('staticfiles.json'),
+            mock.call().__enter__(),
+            mock.call().read(),
+            mock.call().__exit__(None, None, None),
+        ]
+
+    assert mock_call_command.called
+
+    assert mock_storage.mock_calls == [
+        # public
+        mock.call.exists('staticfiles.json'),
+        mock.call.save('staticfiles.json', mock_open.return_value),
+        # tenant1
+        mock.call.exists('staticfiles.json'),
+        mock.call.save('staticfiles.json', mock_open.return_value),
+        # tenant2
+        mock.call.exists('staticfiles.json'),
+        mock.call.save('staticfiles.json', mock_open.return_value),
+    ]
+
+    # The order is non-deterministic in the following data because of the
+    # threading pool so we can just loop instead to account for eveything.
+    for call in [
+        mock.call.info('Collecting staticfiles for tenant: %s', tenant='public'),
+        mock.call.info(
+            '%s files already exist, those uploads will be skipped for tenant: %s', count=2, tenant='public'
+        ),
+        mock.call.info('Uploading the manifest for tenant: %s', tenant='public'),
+        mock.call.info('Collecting staticfiles for tenant: %s', tenant='tenant1'),
+        mock.call.info(
+            '%s files already exist, those uploads will be skipped for tenant: %s', count=2, tenant='tenant1'
+        ),
+        mock.call.info('Uploading the manifest for tenant: %s', tenant='tenant1'),
+        mock.call.info('Collecting staticfiles for tenant: %s', tenant='tenant2'),
+        mock.call.info(
+            '%s files already exist, those uploads will be skipped for tenant: %s', count=2, tenant='tenant2'
+        ),
+        mock.call.info('Uploading the manifest for tenant: %s', tenant='tenant2'),
+    ]:
+        assert call in mock_logger.mock_calls
+
+
+@mock.patch.dict(os.environ, {'ENV': 'production'})
+@mock.patch('dalme_app.management.commands.collectstatic_tenants.staticfiles_storage')
+@mock.patch('dalme_app.management.commands.collectstatic_tenants.call_command')
+@mock.patch('dalme_app.management.commands.collectstatic_tenants.Tenant')
+@mock.patch('dalme_app.management.commands.collectstatic_tenants.logger')
+def test_collectstatic_tenants_exists_force(mock_logger, mock_tenant, mock_call_command, mock_storage):
+    tenant_1 = mock.MagicMock(spec=Tenant)
+    tenant_1.schema_name = 'tenant1'
+    tenant_2 = mock.MagicMock(spec=Tenant)
+    tenant_2.schema_name = 'tenant2'
+    mock_tenant.objects.all.return_value = [tenant_1, tenant_2]
+    mock_storage.exists.return_value = True
+
+    with mock.patch(
+        'dalme_app.management.commands.collectstatic_tenants.Path.open', mock.mock_open(read_data=MANIFEST_DATA)
+    ) as mock_open, mock.patch(
+        'dalme_app.management.commands.collectstatic_tenants.staticfiles_storage.open',
+        mock.mock_open(read_data=MANIFEST_DATA),
+    ) as mock_storage_open:
+        CollectstaticTenants().handle(force=True)
+
+        assert mock_open.mock_calls == [
+            # public
+            mock.call('rb'),
+            mock.call().__enter__(),
+            mock.call().read(),
+            mock.call().__exit__(None, None, None),
+            mock.call('rb'),
+            mock.call().__enter__(),
+            mock.call().__exit__(None, None, None),
+            mock.call('rb'),
+            mock.call().__enter__(),
+            mock.call().__exit__(None, None, None),
+            mock.call('rb'),
+            mock.call().__enter__(),
+            mock.call().__exit__(None, None, None),
+            # tenant1
+            mock.call('rb'),
+            mock.call().__enter__(),
+            mock.call().read(),
+            mock.call().__exit__(None, None, None),
+            mock.call('rb'),
+            mock.call().__enter__(),
+            mock.call().__exit__(None, None, None),
+            mock.call('rb'),
+            mock.call().__enter__(),
+            mock.call().__exit__(None, None, None),
+            mock.call('rb'),
+            mock.call().__enter__(),
+            mock.call().__exit__(None, None, None),
+            # tenant2
+            mock.call('rb'),
+            mock.call().__enter__(),
+            mock.call().read(),
+            mock.call().__exit__(None, None, None),
+            mock.call('rb'),
+            mock.call().__enter__(),
+            mock.call().__exit__(None, None, None),
+            mock.call('rb'),
+            mock.call().__enter__(),
+            mock.call().__exit__(None, None, None),
+            mock.call('rb'),
+            mock.call().__enter__(),
+            mock.call().__exit__(None, None, None),
+        ]
+        assert mock_storage_open.mock_calls == [
+            mock.call('staticfiles.json'),
+            mock.call().__enter__(),
+            mock.call().read(),
+            mock.call().__exit__(None, None, None),
+            mock.call('staticfiles.json'),
+            mock.call().__enter__(),
+            mock.call().read(),
+            mock.call().__exit__(None, None, None),
+            mock.call('staticfiles.json'),
+            mock.call().__enter__(),
+            mock.call().read(),
+            mock.call().__exit__(None, None, None),
+        ]
+
+    assert mock_call_command.called
+
+    # The order is non-deterministic in the following data because of the
+    # threading pool so we can just loop instead to account for eveything.
+    for call in [
+        mock.call.exists('staticfiles.json'),
+        mock.call.save('some/css/file.a2194c262648.css', mock_open.return_value),
+        mock.call.save('some/js/file.034cc7d8a67f.js', mock_open.return_value),
+        mock.call.save('staticfiles.json', mock_open.return_value),
+    ]:
+        assert call in mock_storage.mock_calls
+
+    for call in [
+        mock.call.info('Collecting staticfiles for tenant: %s', tenant='public'),
+        mock.call.info('Forcing the reupload of all staticfiles for tenant: %s', tenant='public'),
+        mock.call.info('Uploading %s files for tenant: %s', count=2, tenant='public'),
+        mock.call.info('File was uploaded to: %s/%s', tenant='public', path='some/js/file.034cc7d8a67f.js'),
+        mock.call.info('File was uploaded to: %s/%s', tenant='public', path='some/css/file.a2194c262648.css'),
+        mock.call.info('Uploading the manifest for tenant: %s', tenant='public'),
+        mock.call.info('Collecting staticfiles for tenant: %s', tenant='tenant1'),
+        mock.call.info('Forcing the reupload of all staticfiles for tenant: %s', tenant='tenant1'),
+        mock.call.info('Uploading %s files for tenant: %s', count=2, tenant='tenant1'),
+        mock.call.info('File was uploaded to: %s/%s', tenant='tenant1', path='some/js/file.034cc7d8a67f.js'),
+        mock.call.info('File was uploaded to: %s/%s', tenant='tenant1', path='some/css/file.a2194c262648.css'),
+        mock.call.info('Uploading the manifest for tenant: %s', tenant='tenant1'),
+        mock.call.info('Collecting staticfiles for tenant: %s', tenant='tenant2'),
+        mock.call.info('Forcing the reupload of all staticfiles for tenant: %s', tenant='tenant2'),
+        mock.call.info('Uploading %s files for tenant: %s', count=2, tenant='tenant2'),
+        mock.call.info('File was uploaded to: %s/%s', tenant='tenant2', path='some/js/file.034cc7d8a67f.js'),
+        mock.call.info('File was uploaded to: %s/%s', tenant='tenant2', path='some/css/file.a2194c262648.css'),
+        mock.call.info('Uploading the manifest for tenant: %s', tenant='tenant2'),
+    ]:
+        assert call in mock_logger.mock_calls
