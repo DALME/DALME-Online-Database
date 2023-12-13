34f3f36d9cff2f43d6ed1fce828c4290f6c71091
jhrr <jhrr@users.noreply.github.com>
2023-12-13 15:08:45 +0000

    Allow the group properties table to be (optionally) scoped to tenants

--- ida/models/group.py
+++ ida/models/group.py
@@ -27,7 +27,7 @@ class GroupProperties(models.Model):
         (WEBSITE, 'Website'),
     )
 
-    # tenant = models.ForeignKey('ida.Tenant', on_delete=models.PROTECT, null=True)
+    tenant = models.ForeignKey('ida.Tenant', on_delete=models.PROTECT, null=True)
     group = models.OneToOneField('auth.Group', on_delete=models.CASCADE, related_name='properties')
     group_type = models.IntegerField(choices=GROUP_TYPES)
     description = models.CharField(max_length=255)
