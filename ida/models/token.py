563c8a71b888e975dc6c7f64468d9b3797dd06ea
jhrr <jhrr@users.noreply.github.com>
2023-12-13 18:00:06 +0000

    Transfer the token model to the ida

--- ida/models/token.py
+++ ida/models/token.py
@@ -9,13 +9,15 @@ options.DEFAULT_NAMES = (*options.DEFAULT_NAMES, 'in_db')
 
 
 class Token(dalmeUuid):
-    object_phrase_id = models.ForeignKey('EntityPhrase', to_field='id', db_index=True, on_delete=models.CASCADE)
+    object_phrase_id = models.ForeignKey(
+        'dalme_app.EntityPhrase', to_field='id', db_index=True, on_delete=models.CASCADE
+    )
     wordform_id = models.ForeignKey('ida.Wordform', to_field='id', db_index=True, on_delete=models.PROTECT)
     raw_token = models.CharField(max_length=255)
     clean_token = models.CharField(max_length=55)
     order = models.IntegerField(db_index=True)
     flags = models.CharField(max_length=10)
-    tags = GenericRelation('Tag')
+    tags = GenericRelation('dalme_app.Tag')
 
     def __str__(self):
         return self.raw_token
