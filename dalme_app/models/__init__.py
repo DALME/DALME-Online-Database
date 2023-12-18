81bc657107b23349c5fd4d203751d0bda358b3b3
jhrr <jhrr@users.noreply.github.com>
2023-12-18 16:37:30 +0000

    Transfer the resource space models to the ida

--- dalme_app/models/__init__.py
+++ dalme_app/models/__init__.py
@@ -1,22 +1,12 @@
 """Interface for the dalme_app.models module."""
-from .resourcespace import (
-    rs_api_query,
-    rs_collection,
-    rs_collection_resource,
-    rs_resource,
-    rs_resource_data,
-    rs_resource_type_field,
-    rs_user,
-)
-
-from .attachment import Attachment  # isort: skip
-from .comment import Comment  # isort: skip
-from .collection import Collection, CollectionMembership  # isort: skip
-from .saved_search import SavedSearch  # isort: skip
-from .scoped import ScopedBase  # isort: skip
-from .tag import Tag  # isort: skip
-from .task import Task, TaskList  # isort: skip
-from .workflow import Workflow, WorkLog  # isort: skip
+from .attachment import Attachment
+from .collection import Collection, CollectionMembership
+from .comment import Comment
+from .saved_search import SavedSearch
+from .scoped import ScopedBase
+from .tag import Tag
+from .task import Task, TaskList
+from .workflow import Workflow, WorkLog
 
 __all__ = [
     'Attachment',
@@ -24,10 +14,6 @@ __all__ = [
     'CollectionMembership',
     'Comment',
     'Location',
-    'Object',
-    'ObjectAttribute',
-    'Permission',
-    'RightsPolicy',
     'SavedSearch',
     'ScopedBase',
     'Tag',
@@ -35,11 +21,4 @@ __all__ = [
     'TaskList',
     'WorkLog',
     'Workflow',
-    'rs_api_query',
-    'rs_collection',
-    'rs_collection_resource',
-    'rs_resource',
-    'rs_resource_data',
-    'rs_resource_type_field',
-    'rs_user',
 ]
