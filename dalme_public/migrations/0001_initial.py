39b49624af30e3756bf01302714c34eb9ca320d7
jhrr <jhrr@users.noreply.github.com>
2024-02-15 17:27:11 +0000

    Refactor incorrect schema and data migrations

--- dalme_public/migrations/0001_initial.py
+++ dalme_public/migrations/0001_initial.py
@@ -26,7 +26,7 @@ class Migration(migrations.Migration):
         ('wagtailcore', '0089_log_entry_data_json_null_to_object'),
         ('taggit', '0005_auto_20220424_2025'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('dalme_app', '0016_attachment_tenant_collection_tenant_and_more'),
+        ('dalme_app', '0018_auto_20240215_1214.py'),
     ]
 
     operations = [
