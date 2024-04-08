fe560eb87ff465fa4434dee35d105919a481140d
Gabe Pizzorno <pizzorno@fas.harvard.edu>
2024-04-08 09:32:00 -0400

    extension-related changes

--- public/templatetags/get_header_image_styles.py
+++ public/templatetags/get_header_image_styles.py
@@ -2,20 +2,24 @@
 
 from django import template
 
+from ida.context import get_gradient_pages
+
 register = template.Library()
 
 
 @register.simple_tag(takes_context=True)
 def get_header_image_styles(context, header_image, header_position):
     page = context['page']
-    if hasattr(page, 'gradient'):
-        gradient = page.gradient.css
-    else:
-        section = page.get_ancestors().filter(content_type_id__in=[118, 123, 126])
-        if section.exists():
-            gradient = section.first().specific_deferred.gradient.css
+    gradient = 'linear-gradient(59deg, #11587c 54.62%, #1b1b1b)'
+    try:
+        if hasattr(page, 'gradient'):
+            gradient = page.gradient.css
         else:
-            gradient = 'linear-gradient(59deg, #11587c 54.62%, #1b1b1b)'
+            section = page.get_ancestors().filter(content_type_id__in=get_gradient_pages())
+            if section.exists():
+                gradient = section.first().specific_deferred.gradient.css
+    except AttributeError:
+        pass
 
     background_image = f'background-image: {gradient}, url({header_image.url})'
     return f'{background_image}; background-size: cover; background-position-y: {header_position}; width: 100%;'
--- public/wagtail_hooks/link_converter_rule.py
+++ public/wagtail_hooks/link_converter_rule.py
@@ -1,6 +1,6 @@
 """Change link converter rules to enable saved search and reference add-ons."""
 
-from public.extensions import LINK_FROM_RULES_REGISTER, LINK_HANDLERS_REGISTER, link_entity_decorator
+# from public.extensions import LINK_FROM_RULES_REGISTER, LINK_HANDLERS_REGISTER, link_entity_decorator
 
 # def link_converter_rule(features):
 #     del features.converter_rules_by_converter['contentstate']['link']
@@ -21,13 +21,38 @@ from public.extensions import LINK_FROM_RULES_REGISTER, LINK_HANDLERS_REGISTER, 
 #     )
 #     features.register_link_type(SavedSearchLinkHandler)
 #     # features.register_link_type(ReferenceLinkHandler)
+import re
+
+from draftjs_exporter.dom import DOM
+
+UUIDv4 = r'^[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-4[0-9A-Fa-f]{3}-[89ABab][0-9A-Fa-f]{3}-[0-9A-Fa-f]{12}$'
+
+
+def link_entity_decorator(props):
+    id_ = props.get('id')
+    link_props = {}
+
+    if id_ is not None and id_ != '':
+        if str(id_).isdigit():
+            link_props['linktype'] = 'page'
+        elif re.fullmatch(UUIDv4, id_):
+            link_props['linktype'] = 'saved_search'
+        # else:
+        #     link_props['linktype'] = 'reference'
+
+        link_props['id'] = id_
+
+    else:
+        link_props['href'] = props.get('url')
+
+    return DOM.create_element('a', link_props, props['children'])
 
 
 def link_converter_rule(features):
     # change converter rules
     from_rules = features.converter_rules_by_converter['contentstate']['link'].get('from_database_format', {})
-    for ruleset in LINK_FROM_RULES_REGISTER:
-        from_rules[ruleset[0]] = ruleset[1]
+    # for ruleset in LINK_FROM_RULES_REGISTER:
+    #     from_rules[ruleset[0]] = ruleset[1]
     del features.converter_rules_by_converter['contentstate']['link']
     features.register_converter_rule(
         'contentstate',
@@ -40,5 +65,5 @@ def link_converter_rule(features):
         },
     )
     # register handlers
-    for link_handler in LINK_HANDLERS_REGISTER:
-        features.register_link_type(link_handler)
+    # for link_handler in LINK_HANDLERS_REGISTER:
+    #     features.register_link_type(link_handler)
