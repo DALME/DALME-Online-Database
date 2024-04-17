01ee7ed97c043c8aaee3a9cfc9e95e401d9f1215
Gabe Pizzorno <pizzorno@fas.harvard.edu>
2024-04-17 17:17:56 -0400

    added frontend startup to set pinned toolbar

--- public/wagtail_hooks/admin_extra_css_js.py
+++ public/wagtail_hooks/admin_extra_css_js.py
@@ -20,7 +20,9 @@ def extra_admin_js():
         <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.full.min.js" integrity="sha512-RtZU3AyMVArmHLiW0suEZ9McadTdegwbgtiQl5Qqo9kunkVg1ofwueXD8/8wv3Af8jkME3DDe3yLfR8HSJfT2g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script> \
         <script type="text/javascript">window.cite = require("citation-js")</script>\
         <script type="text/javascript" src="{}"></script>\
+        <script type="text/javascript" src="{}"></script>\
         <script type="text/javascript">window.APIURL = "{}"</script>',
         static('js/wagtailUtils.js'),
+        static('js/wagtailStartup.js'),
         settings.API_URL,
     )
---
+++ static/js/wagtailStartup.js
@@ -0,0 +1,4 @@
+// startup functions for CMS
+if (!window.localStorage.getItem("wagtail:draftail-toolbar")) {
+  window.localStorage.setItem("wagtail:draftail-toolbar", "sticky");
+}
