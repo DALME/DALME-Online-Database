fae9e9abf55ef38810105c45a1f2520f28b9e28f
Gabe Pizzorno <pizzorno@fas.harvard.edu>
2024-04-20 17:41:01 -0400

    moved records-related forms to records extension

--- public/forms/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Interface for the public.forms module."""
-
-from .contact import ContactForm
-from .record_filter import RecordFilterForm
-
-__all__ = [
-    'ContactForm',
-    'RecordFilterForm',
-]
