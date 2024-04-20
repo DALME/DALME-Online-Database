eb94119b5fb128c06802f96e77883acbe22e9847
Gabe Pizzorno <pizzorno@fas.harvard.edu>
2024-04-20 17:45:32 -0400

    added record mixin and factory to use in pages

--- public/extensions/records/models.py
+++ public/extensions/records/models.py
@@ -2,11 +2,14 @@
 
 from modelcluster.fields import ParentalKey, ParentalManyToManyField
 from modelcluster.models import ClusterableModel
-from wagtail.admin.panels import FieldPanel
+from wagtail.admin.panels import FieldPanel, FieldRowPanel
 from wagtail.fields import RichTextField
 from wagtail.models import Orderable
 
+# from django.core.exceptions import ValidationError
 from django.db import models
+
+from ida.models import Collection, Record
 
 
 class Corpus(Orderable, ClusterableModel):
@@ -27,3 +30,44 @@ class Corpus(Orderable, ClusterableModel):
 
     def __str__(self):
         return self.title
+
+
+def record_mixin_factory(blank=True):
+    class RecordMixin(models.Model):
+        record = models.ForeignKey(
+            Record,
+            null=True,
+            blank=blank,
+            on_delete=models.DO_NOTHING,
+            verbose_name='Linked record',
+            help_text='A record to associate with this page.',
+        )
+        record_collection = models.ForeignKey(
+            Collection,
+            on_delete=models.SET_NULL,
+            null=True,
+            blank=True,
+            verbose_name='Linked collection',
+            help_text='An optional collection associated with this page. The record selected above must be a member of the collection or the page will not validate.',
+        )
+
+        content_panels = [
+            FieldRowPanel(
+                [
+                    FieldPanel('record'),
+                    FieldPanel('record_collection'),
+                ],
+                heading='Linked Record',
+            ),
+        ]
+
+        class Meta:
+            abstract = True
+
+        # def clean(self):
+        #     if self.record_collection and self.record and not self.record.in_collection(self.record_collection):
+        #         msg = f'Record "{self.record}" is not a member of collection "{self.record_collection}"'
+        #         raise ValidationError(msg)
+        #     return super().clean()
+
+    return RecordMixin
