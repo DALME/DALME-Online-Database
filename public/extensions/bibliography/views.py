d8f66dc2fbae23289e5085d0035bdab6b2d9b838
Gabe Pizzorno <pizzorno@fas.harvard.edu>
2024-04-27 14:14:22 -0400

    renamed menu item

--- public/extensions/bibliography/views.py
+++ public/extensions/bibliography/views.py
@@ -47,7 +47,7 @@ class BiblioViewSet(ModelViewSet):
     model = ZoteroCollection
     add_to_reference_index = False
     icon = 'book'
-    menu_label = 'Zotero Collections'
+    menu_label = 'Zotero'
     menu_name = 'biblio'
     menu_order = 900
     add_to_admin_menu = True
