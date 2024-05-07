918da52afff4bc41cdc02b03ee43696ccd0c10ac
Gabe Pizzorno <pizzorno@fas.harvard.edu>
2024-05-07 14:08:53 -0400

    fixes and improvements to multiselect widget

--- public/extensions/team/static/js/user-select-widget.js
+++ public/extensions/team/static/js/user-select-widget.js
@@ -4,6 +4,9 @@ window.CustomUtils.userSelectState = {
     placeholder: "",
     handleFormFields: false,
     avatarUrl: "",
+    photoChooser: null,
+    selectEl: null,
+    nameField:null,
     iconPlaceholder: '<svg class="icon icon-user user-placeholder" aria-hidden="true"><use href="#icon-user"></use></svg>',
     getAvatarHTML: (url) => `<div class="avatar-bg" style="background: center/cover url(${url});"></div>`,
     getFormattedItem: (item) => {
@@ -20,13 +23,32 @@ window.CustomUtils.userSelectState = {
       .then(data => {
           data.forEach((item) => {
             results.push({
-              id: item.id,
+              id: `${item.id}`,
               text: store.getFormattedItem(item),
               item: item,
             });
           });
           callback(results);
       });
+    },
+    togglePreview: () => {
+      const store = window.CustomUtils.userSelectState.store;
+      store.photoChooser.querySelector(".chooser__image").src = store.avatarUrl ? store.avatarUrl : "#";
+      if (!store.photoChooser.classList.contains("blank") || store.avatarUrl) {
+        store.photoChooser.classList.toggle("blank");
+      }
+    },
+    toggleForm: () => {
+      const store = window.CustomUtils.userSelectState.store;
+      const value = store.selectEl.select2("data");
+      if (value) {
+        if (!store.nameField.value) store.nameField.value = value.item.name;
+        store.avatarUrl = value.item.avatar ? value.item.avatar : "";
+      } else {
+        store.nameField.value = "";
+        store.avatarUrl = "";
+      }
+      store.togglePreview();
     },
   },
   queryAPI: (options) => {
@@ -43,38 +65,20 @@ window.CustomUtils.userSelectState = {
   initialFormatter: (el, callback) => {
     const store = window.CustomUtils.userSelectState.store;
     const id_list = store.getIdList(el.val());
+    el.val(id_list);
     store.fetchResults(`${store.baseApiUrl}?id__in=${id_list}`, (results) => {
-      el.val('');
-      callback(results);
+      callback(el.multiple ? results : results[0]);
+      store.toggleForm();
     });
   },
   connectCallback: (selectEl) => {
     const store = window.CustomUtils.userSelectState.store;
     store.handleFormFields = typeof selectEl.data("handle-form-fields") !== "undefined";
     if (store.handleFormFields) {
-      const photoChooser = document.getElementById("id_photo-chooser");
-      const nameField = document.getElementById("id_name");
-
-      const togglePreview = () => {
-        photoChooser.querySelector(".chooser__image").src = store.avatarUrl ? store.avatarUrl : "#";
-        if (!photoChooser.classList.contains("blank") || store.avatarUrl) {
-          photoChooser.classList.toggle("blank");
-        }
-      }
-
-      const toggleForm = () => {
-        const value = selectEl.select2("data");
-        if (value) {
-          nameField.value = value.item.name;
-          store.avatarUrl = value.item.avatar ? value.item.avatar : "";
-        } else {
-          nameField.value = "";
-          store.avatarUrl = "";
-        }
-        togglePreview();
-      }
-
-      selectEl.on("change", toggleForm);
+      store.selectEl = selectEl;
+      store.photoChooser = document.getElementById("id_photo-chooser");
+      store.nameField = document.getElementById("id_name");
+      selectEl.on("change", store.toggleForm);
     }
   },
   disconnectCallback: (selectEl) => {
