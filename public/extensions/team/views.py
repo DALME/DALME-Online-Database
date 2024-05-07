ae597552b8876fb2e27b7c069dcb93f82589ed7c
Gabe Pizzorno <pizzorno@fas.harvard.edu>
2024-05-07 16:58:48 -0400

    added sorting to viewset

--- public/extensions/team/views.py
+++ public/extensions/team/views.py
@@ -57,6 +57,7 @@ class TeamMemberViewSet(ModelViewSet):
     list_filter = ['name', 'title', 'affiliation']
     search_fields = ['name', 'title', 'affiliation', 'biography']
     index_results_template_name = 'team_list/index_results.html'
+    ordering = ['name']
 
     general_panels = [
         FieldRowPanel(
