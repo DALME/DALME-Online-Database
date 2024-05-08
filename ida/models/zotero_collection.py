ca20b8ff2dd7b259650ec04d8d559c942fac270c
Gabe Pizzorno <pizzorno@fas.harvard.edu>
2024-05-08 09:10:35 -0400

    fixed typo

--- ida/models/zotero_collection.py
+++ ida/models/zotero_collection.py
@@ -10,7 +10,7 @@ class ZoteroCollection(models.Model):
     """Stores information about Zotero collections associated with a project."""
 
     id = models.CharField(primary_key=True, max_length=25, help_text='Zotero ID of the collection.')
-    project = models.ForeignKey('ida.Project', on_delete=models.CASCADE, related_name='zoter_collections')
+    project = models.ForeignKey('ida.Project', on_delete=models.CASCADE, related_name='zotero_collections')
     label = models.CharField(max_length=255, help_text='Label for the collection.')
     has_biblio_sources = models.BooleanField(
         default=False,
