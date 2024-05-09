e67d51d700f938f14eaa98a402dae40ae6a02e27
Gabe Pizzorno <pizzorno@fas.harvard.edu>
2024-05-09 14:41:25 -0400

    added help text + made page optional

--- public/extensions/banners/models.py
+++ public/extensions/banners/models.py
@@ -18,7 +18,7 @@ COLOUR_CHOICES = {
 class Banner(models.Model):
     id = models.AutoField(primary_key=True, unique=True, db_index=True)
     title = models.CharField(max_length=255, help_text='The title of the banner.')
-    show_title = models.BooleanField(default=True, verbose_name='Show?')
+    show_title = models.BooleanField(default=True, verbose_name='Show', help_text='title?')
     info = RichTextField(
         features=[
             'bold',
@@ -39,13 +39,19 @@ class Banner(models.Model):
         on_delete=models.SET_NULL,
         related_name='banner',
         null=True,
+        blank=True,
         help_text='If selected, a "Learn more..." button linking to a page will be included in the banner.',
     )
     url = models.URLField(
         blank=True,
         help_text='If included a link to the URL will be added to the title of the banner.',
     )
-    color = models.CharField(max_length=10, default='default', choices=COLOUR_CHOICES)
+    color = models.CharField(
+        max_length=10,
+        default='default',
+        choices=COLOUR_CHOICES,
+        help_text='Choose scheme.',
+    )
     start_date = models.DateField()
     end_date = models.DateField()
 
