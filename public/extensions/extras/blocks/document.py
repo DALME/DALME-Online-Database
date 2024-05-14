2045ee07e1b6c39ebd033b430a26a8337dbf83d8
Gabe Pizzorno <pizzorno@fas.harvard.edu>
2024-05-14 12:57:10 -0400

    refactored document block

--- public/extensions/extras/blocks/document.py
+++ public/extensions/extras/blocks/document.py
@@ -11,17 +11,26 @@ class DocumentBlock(blocks.StructBlock):
             ('publication', 'Publication'),
             ('talk', 'Talk'),
         ],
+        help_text='Select type of document.',
     )
-    title = blocks.CharBlock()
-    abstract = blocks.CharBlock(required=False)
-    author = blocks.CharBlock()
-    detail = blocks.CharBlock(required=False)
-    version = blocks.FloatBlock(required=False)
-    document = DocumentChooserBlock(required=False)
-    url = blocks.URLBlock(required=False)
-    page = blocks.PageChooserBlock(required=False)
-    date = blocks.DateBlock()
+    version = blocks.DecimalBlock(required=False, help_text='Version of the document, if applicable.')
+    date = blocks.DateBlock(help_text='Date for this entry (e.g. creation, publication, etc.')
+    title = blocks.CharBlock(help_text='Title for this entry.')
+    author = blocks.CharBlock(help_text='Name of the author(s) for this entry.')
+    detail = blocks.CharBlock(
+        required=False,
+        help_text='Additional information, e.g. press/place of publication, location of the talk, etc.',
+    )
+    abstract = blocks.RichTextBlock(
+        required=False,
+        help_text='Abstract for this entry, if applicable.',
+    )
+    url = blocks.URLBlock(required=False, help_text='Related URL, e.g. link to online resource, press site, etc.')
+    document = DocumentChooserBlock(required=False, help_text='Document file (e.g. PDF).')
+    page = blocks.PageChooserBlock(required=False, help_text='Add a link to a page on this site.')
 
     class Meta:
         icon = 'file-pdf'
         template = 'document_block.html'
+        form_classname = 'struct-block document-block'
+        form_template = 'document_form.html'
