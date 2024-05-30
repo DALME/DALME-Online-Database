bbd42911c94e931efe809b965bce5f49ef4c2225
Gabe Pizzorno <pizzorno@fas.harvard.edu>
2024-05-30 12:31:14 -0400

    fixed issue with lightbox not triggering

--- public/extensions/images/static/js/images.js
+++ public/extensions/images/static/js/images.js
@@ -1,5 +1,5 @@
 window.CustomUtils.toggleLightbox = (el) => {
   document.querySelector(".lightbox-image").src = el.querySelector("img").src;
   document.querySelector(".lightbox-caption").innerHTML = el.querySelector(".caption").innerHTML;
-  document.getElementById("lightbox").modal("show");
+  $("#lightbox").modal("show");
 };
