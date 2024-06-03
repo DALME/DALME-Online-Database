9e2414d31d3b0f3e770b9a1bade06e59ec94d728
Gabe Pizzorno <pizzorno@fas.harvard.edu>
2024-06-03 16:15:57 -0400

    improvements to dynamic serializer

--- api/dynamic_serializer.py
+++ api/dynamic_serializer.py
@@ -13,16 +13,22 @@ class DynamicSerializer(serializers.ModelSerializer):
 
         fields = kwargs.pop('fields', None)
         field_set = kwargs.pop('field_set', None)
+
         super().__init__(*args, **kwargs)
 
         if field_set is not None:
-            assert hasattr(self.Meta, 'field_sets'), (
-                'Use of `field_set` requires the `field_sets` dictionary to be defined in the class `Meta`.',
-            )
-            fields = self.Meta.field_sets.get(field_set)
+            assert hasattr(self.Meta, 'field_sets'), 'Use of `field_set` requires `field_sets` dictionary in `Meta`.'
+            if isinstance(field_set, (list | tuple)):
+                fields = [f for i in field_set for f in self.Meta.field_sets.get(i)]
+            else:
+                fields = self.Meta.field_sets.get(field_set)
 
         if fields is not None:
             set_fields = dict(self.fields)
             for k, _v in set_fields.items():
                 if k not in fields:
                     self.fields.pop(k)
+
+        elif hasattr(self.Meta, 'default_exclude'):
+            for field in self.Meta.default_exclude:
+                self.fields.pop(field)
