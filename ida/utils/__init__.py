b62d45a104f5d8d2a0d513c253de3c883ec0194c
Gabe Pizzorno <pizzorno@fas.harvard.edu>
2024-06-06 09:18:38 -0400

    removed old IDADate class and other unused utilities

--- ida/utils/__init__.py
+++ ida/utils/__init__.py
@@ -1,20 +1,15 @@
 """Interface for the ida.utils module."""
 
 from .database_router import ModelDatabaseRouter
-from .date_time_helpers import IDADate, round_timesince
 from .domain_middleware import SubdomainRedirectMiddleware
-from .offline_context_generator import offline_context_generator
 from .paginated_formsets import formset_factory
 from .search import Search, SearchContext
 
 __all__ = [
-    'IDADate',
     'ModelDatabaseRouter',
     'Search',
     'SearchContext',
     'SubdomainRedirectMiddleware',
     'TENANT',
     'formset_factory',
-    'offline_context_generator',
-    'round_timesince',
 ]
