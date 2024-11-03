b394bbfb164a9a2461cd32ac68e9d4fd884a96d0
jhrr <jhrr@users.noreply.github.com>
2024-11-03 19:52:20 +0000

    Remove problematic 'casts' migration

--- ida/migrations/0003_tenant_tenant_type.py
+++ ida/migrations/0003_tenant_tenant_type.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ('ida', '0002_casts'),
+        ('ida', '0001_initial'),
     ]
 
     operations = [
