3ca7c3e1c528da5a8a458a769bab13dad8f3aae9
jhrr <jhrr@users.noreply.github.com>
2025-02-15 13:14:42 +0000

    Make sure reset sequences is actually a management command

--- app/domain/management/commands/reset_sequences.py
+++ app/domain/management/commands/reset_sequences.py
@@ -2,18 +2,29 @@
 
 import os
 
-os.environ['DJANGO_COLORS'] = 'nocolor'
-
+import structlog
 from StringIO import StringIO
 
 from django.apps import apps
 from django.core.management import call_command
+from django.core.management.base import BaseCommand
 from django.db import connection
 
-commands = StringIO()
-cursor = connection.cursor()
+os.environ['DJANGO_COLORS'] = 'nocolor'
 
-for app in apps.get_app_configs():
-    call_command('sqlsequencereset', app.label, stdout=commands)
+logger = structlog.get_logger(__name__)
 
-cursor.execute(commands.getvalue())
+
+class Command(BaseCommand):
+    """Define the reset_sequences command."""
+
+    def handle(self, *args, **options):  # noqa: ARG002
+        """Execute the SQL to reset db table sequences."""
+        commands = StringIO()
+        cursor = connection.cursor()
+
+        for app in apps.get_app_configs():
+            call_command('sqlsequencereset', app.label, stdout=commands)
+            logger.info('Resetting sequences for app: %s', app=app.label)
+
+        cursor.execute(commands.getvalue())
