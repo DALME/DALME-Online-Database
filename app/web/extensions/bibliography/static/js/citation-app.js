f1a9c419a63647586925a04e963315ad8723206d
jhrr <jhrr@users.noreply.github.com>
2025-02-19 10:33:39 +0000

    Swtich all staticfiles path references from 'common' to 'web'

--- app/web/extensions/bibliography/static/js/citation-app.js
+++ app/web/extensions/bibliography/static/js/citation-app.js
@@ -55,7 +55,7 @@ return {
               that.config.templates.add(style.name, xml_data);
             }
           };
-          xhttp.open("GET", `/static/common/citation_styles/${style.file}`, true);
+          xhttp.open("GET", `/static/web/citation_styles/${style.file}`, true);
           xhttp.send();
         }
       }));
