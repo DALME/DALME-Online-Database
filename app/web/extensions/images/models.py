d0d0a6316a05079cf963ddbea0cd3a64e9c2f04e
Gabe Pizzorno <pizzorno@fas.harvard.edu>
2025-02-20 10:10:50 -0500

    remove unnecessary override of field length

--- app/web/extensions/images/models.py
+++ app/web/extensions/images/models.py
@@ -1,14 +1,6 @@
 """Models for images extension."""
 
-from wagtail.images.models import (
-    AbstractImage,
-    AbstractRendition,
-    Image,
-    WagtailImageField,
-    get_rendition_storage,
-    get_rendition_upload_to,
-    get_upload_to,
-)
+from wagtail.images.models import AbstractImage, AbstractRendition, Image
 
 from django.db import models
 
@@ -16,27 +8,10 @@ from django.db import models
 class BaseImage(AbstractImage):
     caption = models.CharField(max_length=255, null=True, blank=True)
     admin_form_fields = (*Image.admin_form_fields, 'caption')
-    # we need to override the image field because by default it is limited
-    # to 100 characters and certain existing images go over when we add the
-    # tenant prefix in data migrations
-    file = WagtailImageField(
-        verbose_name='file',
-        upload_to=get_upload_to,
-        width_field='width',
-        height_field='height',
-        max_length=255,
-    )
 
 
 class CustomRendition(AbstractRendition):
     image = models.ForeignKey(BaseImage, on_delete=models.CASCADE, related_name='renditions')
-    file = WagtailImageField(
-        upload_to=get_rendition_upload_to,
-        storage=get_rendition_storage,
-        width_field='width',
-        height_field='height',
-        max_length=255,
-    )
 
     class Meta:
         unique_together = ('image', 'filter_spec', 'focal_point_key')
