2353d92731b71b11b0e5ede2d61404472fa18b5c
jhrr <jhrr@users.noreply.github.com>
2025-02-22 12:49:10 +0000

    Revert the last storage backend experiment

--- app/tenants/storage_backends.py
+++ app/tenants/storage_backends.py
@@ -12,49 +12,45 @@ from app.context import get_current_tenant
 logger = structlog.get_logger(__name__)
 
 
-class StaticStorage(S3ManifestStaticStorage):
+class TenantStorageMixin:
+    """Reusable mutltitenant storage functionality."""
+
+    def get_default_settings(self):
+        settings = super().get_default_settings()
+        settings['location'] = self.location
+        return settings
+
+    @property
+    def schema(self):
+        """Get the tenant schema name."""
+        try:
+            return get_current_tenant().schema_name
+        except RuntimeError:
+            return connection.tenant.schema_name
+
+    @property
+    def location(self):
+        """Get the schema qualified filepath prefix.
+
+        This corresponds the `AWS_LOCATION` setting.
+
+        https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
+
+        """
+        return f'{self.key}/{self.schema}'
+
+
+class StaticStorage(TenantStorageMixin, S3ManifestStaticStorage):
     """Multitenant aware staticfiles storage class for S3."""
 
     key = 'static'
 
-    def get_default_settings(self):
-        settings = super().get_default_settings()
-        settings['location'] = self.location
-        return settings
 
-    @property
-    def location(self):
-        """Get the schema qualified filepath prefix.
-
-        This corresponds the `AWS_LOCATION` setting.
-
-        https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
-
-        """
-        return f'{self.key}/%s'
-
-
-class MediaStorage(S3Boto3Storage):
+class MediaStorage(TenantStorageMixin, S3Boto3Storage):
     """Multitenant aware media files storage class for S3."""
 
     file_overwrite = False
     key = 'media'
-
-    def get_default_settings(self):
-        settings = super().get_default_settings()
-        settings['location'] = self.location
-        return settings
-
-    @property
-    def location(self):
-        """Get the schema qualified filepath prefix.
-
-        This corresponds the `AWS_LOCATION` setting.
-
-        https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
-
-        """
-        return f'{self.key}/%s'
 
 
 class LocalMediaStorage(FileSystemStorage):
