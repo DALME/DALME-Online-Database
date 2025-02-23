916a08b91881e302895a7344bb3b40522269b754
jhrr <jhrr@users.noreply.github.com>
2025-02-23 11:50:06 +0000

    Remove debugging code from storage backend

--- app/tenants/storage_backends.py
+++ app/tenants/storage_backends.py
@@ -26,17 +26,16 @@ class TenantStorageMixin:
 
 
 class StaticStorage(TenantStorageMixin, S3ManifestStaticStorage):
-    """Multitenant aware staticfiles storage class for S3."""
+    """Multitenant aware staticfiles storage class for S3.
+
+    We have to make sure to recompute the manifest data whenever the tenant
+    search path changes because otherwise the manifest is only parsed and read
+    once at application startup time when it will just be pointing to 'public'
+    because it is outside any request/response cycle.
+
+    """
 
     key = 'static'
-
-    # Overrides for strict tenanting over multiple manifests.
-    def read_manifest(self):
-        with self.manifest_storage.open(self.manifest_name) as manifest:
-            data = manifest.read().decode()
-            logger.critical('-------- %s/%s --------', self.location, self.manifest_name)
-            logger.critical(data)
-            return data
 
     def save_manifest(self):
         self._remap_manifest()
