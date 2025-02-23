254338201acd4acd367b7def6358f4588be6ecb0
jhrr <jhrr@users.noreply.github.com>
2025-02-23 10:05:23 +0000

    Return to debugging storage backeneds

--- app/tenants/storage_backends.py
+++ app/tenants/storage_backends.py
@@ -1,5 +1,7 @@
 """Customize staticfiles storage logic."""
 
+import structlog
+from django_tenants.utils import parse_tenant_config_path
 from storages.backends.s3boto3 import S3Boto3Storage, S3ManifestStaticStorage
 
 from django.conf import settings
@@ -8,39 +10,40 @@ from django.db import connection
 
 from app.context import get_current_tenant
 
+logger = structlog.get_logger(__name__)
+
+
+class ManifestStaticStorage(S3ManifestStaticStorage):
+    """Debugging."""
+
+    def read_manifest(self):
+        with self.manifest_storage.open(self.manifest_name) as manifest:
+            data = manifest.read().decode()
+            logger.critical('-------- %s/%s --------', self.location, self.manifest_name)
+            logger.critical(data)
+            return data
+
 
 class TenantStorageMixin:
     """Reusable mutltitenant storage functionality."""
 
     def get_default_settings(self):
         settings = super().get_default_settings()
-        settings['location'] = self.location
+        settings['location'] = self._get_location()
         return settings
 
-    @property
-    def schema(self):
-        """Get the tenant schema name."""
-        try:
-            return get_current_tenant().schema_name
-        except RuntimeError:
-            return connection.tenant.schema_name
-
-    @property
-    def location(self):
-        """Get the schema qualified filepath prefix.
-
-        This corresponds the `AWS_LOCATION` setting.
-
-        https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
-
-        """
-        return f'{self.key}/{self.schema}'
+    def _get_location(self):
+        return parse_tenant_config_path(f'{self.key}/%s')
 
 
 class StaticStorage(TenantStorageMixin, S3ManifestStaticStorage):
     """Multitenant aware staticfiles storage class for S3."""
 
     key = 'static'
+
+    def __init__(self, *args, **kwargs):
+        manifest_storage = ManifestStaticStorage(location=self._get_location())
+        super().__init__(*args, manifest_storage=manifest_storage, **kwargs)
 
 
 class MediaStorage(TenantStorageMixin, S3Boto3Storage):
