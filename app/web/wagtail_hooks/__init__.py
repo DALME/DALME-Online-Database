0b752b764f58b9d84f5a1eccbf9e60ff060f5f7e
Gabe Pizzorno <pizzorno@fas.harvard.edu>
2025-02-26 14:00:10 -0500

    enable get_avatar hook

--- app/web/wagtail_hooks/__init__.py
+++ app/web/wagtail_hooks/__init__.py
@@ -5,8 +5,7 @@ from wagtail import hooks
 from web.views import SnippetsViewSetGroup
 
 from .admin_extra_css_js import extra_admin_css, extra_admin_js
-
-# from .avatar_image import get_avatar
+from .avatar_image import get_avatar
 from .extra_icons import register_extra_icons
 from .redirects_before_serving import add_redirects_before_serving_pages
 
@@ -15,4 +14,4 @@ hooks.register('insert_global_admin_js', extra_admin_js)
 hooks.register('before_serve_page', add_redirects_before_serving_pages)
 hooks.register('register_icons', register_extra_icons)
 hooks.register('register_admin_viewset', SnippetsViewSetGroup)
-# hooks.register('get_avatar_url', get_avatar)
+hooks.register('get_avatar_url', get_avatar)
