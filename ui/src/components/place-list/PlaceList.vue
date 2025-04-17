429b356d6c2ff5de7553b936a0b78dd8cf9ab151
Gabe Pizzorno <pizzorno@fas.harvard.edu>
2025-04-17 08:59:57 -0400

    improvements to place listing

--- ui/src/components/place-list/PlaceList.vue
+++ ui/src/components/place-list/PlaceList.vue
@@ -55,13 +55,27 @@ export default defineComponent({
     const updateRequest = requests.places.inlineUpdate;
     const editable = ["notes"];
 
+    const getLocationDetail = (place) => {
+      if (!place.location) return null;
+      if (place.location.locationType == "Locale") {
+        const name = place.location?.attributes[0]?.value.name;
+        const region = place.location?.attributes[0]?.value.administrativeRegion;
+        const country = place.location?.attributes[0]?.value.country?.name;
+        return `${name}, ${region} (${country})`;
+      }
+    };
+
     const fetchData = async (query) => {
       const request = requests.places.getPlaces(query);
       await fetchAPI(request);
       if (success.value)
-        await placeListSchema.validate(data.value.data, { stripUnknown: true }).then((value) => {
+        await placeListSchema.validate(data.value.data, { stripUnknown: false }).then((value) => {
           columns.value = getColumns(columnMap);
           pagination.value.rowsNumber = data.value.count;
+          value.forEach((p) => {
+            p.locationType = p.location?.locationType || null;
+            p.locationDetail = getLocationDetail(p);
+          });
           rows.value = value;
           loading.value = false;
         });
