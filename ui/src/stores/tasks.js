6c247884c3fce07585751b4730a0d9c4aac18f1c
Gabe Pizzorno <pizzorno@fas.harvard.edu>
2025-04-17 08:42:07 -0400

    rewrote tasks stores using models

--- ui/src/stores/tasks.js
+++ ui/src/stores/tasks.js
@@ -1,58 +1,75 @@
 import { defineStore } from "pinia";
 import { computed, defineAsyncComponent, ref } from "vue";
+import { useAuthStore } from "@/stores/auth";
+import { useRepo } from "pinia-orm";
+import { Task, TaskList } from "@/models";
+import { API as apiInterface, requests } from "@/api";
+import { tasksSchema, taskListsSchema } from "@/schemas";
 import { Dialog } from "quasar";
 import { useRouter } from "vue-router";
-import { useTasklistStore } from "@/stores/tasklist-store";
-import { useTaskStore } from "@/stores/task-store";
-import { filter as rFilter, groupBy, sortBy, prop, descend } from "ramda";
 
-export const useTasks = defineStore("tasks", () => {
-  // stores
-  const _tasks = useTaskStore();
-  const _lists = useTasklistStore();
+export const useTaskStore = defineStore("tasks", () => {
+  // stores and repositories
+  const auth = useAuthStore();
+  const taskRepo = useRepo(Task);
+  const taskListRepo = useRepo(TaskList);
   const $router = useRouter();
 
   // state
-  const viewing = ref(null);
-  const loadAssigned = ref(5);
-  const loadCompleted = ref(5);
-  const loadCreated = ref(5);
+  const ready = ref(false);
+  const timestamp = ref(null);
+  const loading = ref(false);
+  const onViewer = ref(null);
   const activeFilters = ref(new Set());
   const activeGroups = ref(new Set());
   const activeLists = ref(new Set());
   const activeSort = ref("");
-  const show = ref({
-    all: 5,
-    created: 5,
-    assigned: 5,
-    completed: 5,
-  });
-  const presets = ref({
-    assigned: [{ prop: "isAssignee", value: true }],
-    completed: [{ prop: "completed", value: true }],
-    created: [
-      { prop: "isAuthor", value: true },
-      { prop: "isAssignee", value: false },
-      { prop: "completed", value: false },
-    ],
-  });
+  const meta = ref({ all: 0, user: 0, created: 0, assigned: 0, completed: 0 });
+  const showCount = ref({ all: 5, created: 5, assigned: 5, completed: 5 });
+
+  const request = {
+    list: {
+      user: requests.tasks.getUserTaskLists,
+      null: requests.tasks.getTaskLists,
+    },
+    task: {
+      user: requests.tasks.getUserTasks,
+      null: requests.tasks.getTasks,
+      assigned: requests.tasks.getAssignedTasks,
+      completed: requests.tasks.getCompletedTasks,
+      created: requests.tasks.getCreatedTasks,
+    },
+  };
+
+  const schemas = {
+    list: taskListsSchema,
+    task: tasksSchema,
+  };
 
   // getters
-  const loading = computed(() => _tasks.loading || _lists.loading);
-  const tasksReady = computed(() => _tasks.isLoaded && !_tasks.loading);
-  const listsReady = computed(() => _lists.isLoaded && !_lists.loading);
-  const tasks = computed(() => _sortByCreationDate(filter().slice(0, show.value.all)));
-  const assigned = computed(() => filter(presets.value.assigned).slice(0, show.value.assigned));
-  const completed = computed(() => filter(presets.value.completed).slice(0, show.value.completed));
-  const created = computed(() => filter(presets.value.created).slice(0, show.value.created));
-  const moreTasks = computed(() => show.value.all < _tasks.meta.all);
-  const moreAssigned = computed(() => show.value.assigned < _tasks.meta.assigned);
-  const moreCompleted = computed(() => show.value.completed < _tasks.meta.completed);
-  const moreCreated = computed(() => show.value.created < _tasks.meta.created);
-  const listGroups = computed(() => groupBy((x) => x.teamLink.name, _lists.lists));
-  const loads = computed(() => {
+  const isStale = computed(() => Date.now() - timestamp.value > 86400000);
+  const moreTasks = computed(() => showCount.value.all < meta.value.all);
+  const moreAssigned = computed(() => showCount.value.assigned < meta.value.assigned);
+  const moreCompleted = computed(() => showCount.value.completed < meta.value.completed);
+  const moreCreated = computed(() => showCount.value.created < meta.value.created);
+
+  const assigned = computed(() =>
+    taskRepo.where("isAssignee", true).limit(showCount.value.assigned).get(),
+  );
+  const completed = computed(() =>
+    taskRepo.where("completed", true).limit(showCount.value.assigned).get(),
+  );
+  const created = computed(() =>
+    taskRepo
+      .where((q) => q.isAuthor === true && q.isAssignee === false && q.completed === false)
+      .limit(showCount.value.assigned)
+      .get(),
+  );
+  const listGroups = computed(() => taskListRepo.groupBy("teamLink.name").get());
+
+  const loaded = computed(() => {
     return {
-      all: tasks.value.length,
+      all: taskRepo.all().length,
       assigned: assigned.value.length,
       completed: completed.value.length,
       created: created.value.length,
@@ -60,71 +77,123 @@ export const useTasks = defineStore("tasks", () => {
   });
 
   // actions
-  const $reset = () => {
-    _tasks.$reset();
-    _lists.$reset();
+  const initialize = () => {
+    return new Promise((resolve) => {
+      $reset();
+      fetchData(taskListRepo, "list", "user", null, 50).then(() => {
+        fetchData(taskRepo, "task", "user", null, 15).then((data) => {
+          const state = $router.options.history.state;
+          timestamp.value = Date.now();
+          meta.value.all = data.totalTasks;
+          meta.value.user = data.count;
+          meta.value.created = data.totalCreated;
+          meta.value.assigned = data.totalAssigned;
+          meta.value.completed = data.totalCompleted;
+          ready.value = true;
+          loading.value = false;
+          if ("taskId" in state) setViewer(state.taskId);
+          if ("showTasks" in state) showTaskModal();
+          return resolve(true);
+        });
+      });
+    });
   };
 
-  const init = (target = null) => {
-    const state = $router.options.history.state;
-    if (target == "lists") {
-      _lists.init();
-    } else {
-      _tasks.init().then(() => {
-        if (target == null) {
-          _lists.init().then(() => {
-            if ("taskId" in state) setViewing(state.taskId, true);
-            if ("showTasks" in state) showTaskModal();
+  const $reset = () => {
+    ready.value = false;
+    timestamp.value = null;
+    loading.value = false;
+    meta.value = {
+      all: 0,
+      user: 0,
+      created: 0,
+      assigned: 0,
+      completed: 0,
+    };
+  };
+
+  const getRequest = (resource, type, query, limit = 0, offset = 0) => {
+    const target = query ? query : auth.user.userId;
+    return request[resource][type](target, limit, offset);
+  };
+
+  const fetchData = (repository, resource, type = null, query = null, limit = 0, offset = 0) => {
+    return new Promise((resolve) => {
+      const { success, data, fetchAPI } = apiInterface();
+      loading.value = true;
+      const request = getRequest(resource, type, query, limit, offset);
+      const schema = schemas[resource];
+      fetchAPI(request).then(() => {
+        if (success.value) {
+          schema.validate(data.value.data, { stripUnknown: true }).then((response) => {
+            repository.save(response);
+            return resolve(data.value);
           });
         }
       });
-    }
+    });
   };
 
-  const filter = (passedFilters = null) => {
-    if (passedFilters || activeFilters.value.size > 0 || activeLists.value.size > 0) {
-      const pred = (x) => {
-        if (passedFilters) {
-          for (const f of passedFilters) {
-            if (prop(f.prop, x) != f.value) return false;
-          }
-        } else {
-          if (activeLists.value.size > 0 && !activeLists.value.has(x.listSlug)) return false;
-          if (activeFilters.value.size > 0) {
-            for (const f of activeFilters.value) {
-              if (prop(f.prop, x) != f.value) return false;
-            }
-          }
-        }
-        return true;
-      };
-      return rFilter(pred, _tasks.tasks);
+  const tasks = (query = null) => {
+    if (!query) {
+      return taskRepo.orderBy("creationTimestamp").get();
     } else {
-      return _tasks.tasks;
-    }
-  };
-
-  const setFilterList = (value, isGroup = false) => {
-    if (isGroup) {
-      if (activeGroups.value.has(value)) {
-        activeGroups.value.delete(value);
-        _lists.index[value].forEach((x) => activeLists.value.delete(`${value}-${x}`));
-      } else {
-        activeGroups.value.add(value);
-        _lists.index[value].forEach((x) => activeLists.value.add(`${value}-${x}`));
-      }
-    } else {
-      if (activeLists.value.has(value)) {
-        activeLists.value.delete(value);
-      } else {
-        activeLists.value.add(value);
+      if (typeof query === "string") {
+        return taskRepo
+          .where(
+            (q) =>
+              q.title.toLowerCase().includes(query.toLowerCase()) ||
+              q.description.toLowerCase().includes(query.toLowerCase()),
+          )
+          .get();
+      } else if (typeof query === "number") {
+        return taskRepo.find(query);
       }
     }
   };
 
-  // const setFilter = () => {
+  const lists = (query = null) => {
+    if (!query) {
+      return taskListRepo.all();
+    } else {
+      if (typeof query === "string") {
+        return taskListRepo
+          .where(
+            (q) =>
+              q.name.toLowerCase().includes(query.toLowerCase()) ||
+              q.description.toLowerCase().includes(query.toLowerCase()),
+          )
+          .get();
+      } else if (typeof query === "number") {
+        return taskListRepo.find(query);
+      }
+    }
+  };
 
-  // };
+  const setViewer = (task) => {
+    const target = typeof task === "number" ? tasks(task) : task;
+    onViewer.value = target;
+  };
+
+  const showTaskModal = () => {
+    Dialog.create({
+      component: defineAsyncComponent(() => import("components/task-manager/TaskViewer.vue")),
+    });
+  };
+
+  const onShowMore = (type) => {
+    let target =
+      showCount.value[type] + 1 >= meta.value[type]
+        ? meta.value[type]
+        : (showCount.value[type] += 1);
+
+    if (target > loaded.value[type] && loaded.value[type] < meta.value[type] && !loading.value) {
+      fetchData(taskRepo, "task", type, 1, loaded.value[type]).then(() => {
+        showCount.value[type] = target;
+        loading.value = false;
+      });
+    }
+  };
 
   const clearFilters = () => {
     activeFilters.value.clear();
@@ -135,59 +204,50 @@ export const useTasks = defineStore("tasks", () => {
     activeLists.value.clear();
   };
 
-  const setViewing = (task, byId = false) => {
-    const target = byId ? _tasks.getTaskbyId(task) : task;
-    viewing.value = target;
-  };
-
-  const showTaskModal = () => {
-    Dialog.create({
-      component: defineAsyncComponent(() => import("components/task-manager/TaskViewer.vue")),
-    });
-  };
-
-  const onShowMore = (list) => {
-    let target =
-      show.value[list] + 1 >= _tasks.meta[list] ? _tasks.meta[list] : (show.value[list] += 1);
-
-    if (target > loads.value[list] && loads.value[list] < _tasks.meta[list] && !_tasks.loading) {
-      _tasks.fetchData(list, 1, loads.value[list]).then(() => {
-        show.value[list] = target;
-      });
+  const setFilterList = (value, isGroup = false) => {
+    if (isGroup) {
+      if (activeGroups.value.has(value)) {
+        activeGroups.value.delete(value);
+        lists.index[value].forEach((x) => activeLists.value.delete(`${value}-${x}`));
+      } else {
+        activeGroups.value.add(value);
+        lists.index[value].forEach((x) => activeLists.value.add(`${value}-${x}`));
+      }
+    } else {
+      if (activeLists.value.has(value)) {
+        activeLists.value.delete(value);
+      } else {
+        activeLists.value.add(value);
+      }
     }
   };
 
-  const _sortByCreationDate = sortBy(descend(prop("creationTimestamp")));
-  const _sortByCompletedDate = sortBy(descend(prop("completedDate")));
-
   return {
-    tasks,
-    $reset,
-    init,
-    assigned,
-    completed,
-    created,
+    initialize,
+    isStale,
+    ready,
     loading,
-    loadAssigned,
-    loadCompleted,
-    loadCreated,
+    tasks,
+    lists,
     moreTasks,
     moreAssigned,
     moreCompleted,
     moreCreated,
+    setViewer,
+    showTaskModal,
+    meta,
     onShowMore,
-    tasksReady,
-    listsReady,
+    assigned,
+    completed,
+    created,
+    onViewer,
     listGroups,
-    viewing,
-    setViewing,
+    activeFilters,
     activeGroups,
     activeLists,
+    activeSort,
+    setFilterList,
     clearFilters,
     clearListFilters,
-    setFilterList,
-    activeSort,
-    showTaskModal,
-    tasksMeta: _tasks.meta,
   };
 });
