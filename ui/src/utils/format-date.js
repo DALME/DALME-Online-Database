d6ef1edab865ce4ada3b582ff3e65e1cf0d12475
Gabe Pizzorno <pizzorno@fas.harvard.edu>
2025-04-22 08:43:37 -0400

    improved date formatting

--- ui/src/utils/format-date.js
+++ ui/src/utils/format-date.js
@@ -2,15 +2,23 @@ import { useConstants } from "@/use/constants";
 
 const { dateFormats } = useConstants();
 
-export const formatDate = (date, fmt) => {
-  const format = dateFormats[fmt];
-  if (Array.isArray(format)) {
-    let result = format.map((f) => {
-      if (f in dateFormats) return new Intl.DateTimeFormat(undefined, dateFormats[f]).format(date);
-      return f;
-    });
-    return result.join(" ");
-  } else if (typeof format === "object") {
-    return new Intl.DateTimeFormat(undefined, format).format(date);
+export const formatDate = (date, fmt, tz = null) => {
+  if (date instanceof Date && !isNaN(date)) {
+    const format = dateFormats[fmt];
+    if (Array.isArray(format)) {
+      let result = format.map((f) => {
+        if (f in dateFormats) {
+          const sfmt = dateFormats[f];
+          if (tz) sfmt.timeZone = tz;
+          return new Intl.DateTimeFormat(undefined, sfmt).format(date);
+        }
+        return f;
+      });
+      return result.join(" ");
+    } else if (typeof format === "object") {
+      if (tz) format.timeZone = tz;
+      return new Intl.DateTimeFormat(undefined, format).format(date);
+    }
   }
+  return date;
 };
