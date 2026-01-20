bc27eff28024b09ec31a230e0884d0b2aa72fb96
jhrr <jhrr@users.noreply.github.com>
2026-01-20 15:28:43 +0000

    Attempt to force eager evaluation of default storage logic

--- app/web/apps.py
+++ app/web/apps.py
@@ -5,3 +5,9 @@ from django.apps import AppConfig
 
 class WebConfig(AppConfig):
     name = 'web'
+
+    def ready(self):
+        # Force eager resolution of default_storage.
+        from django.core.files.storage import default_storage
+
+        default_storage._setup()  # noqa: SLF001
